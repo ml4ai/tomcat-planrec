2e9676a940de52754c7d60efa634fc46bb658161
Loren Champlin <lorenchamplin@SBS-7185.local>
2021-05-08 17:29:35 -0700

    Added storage for new domain Selector class

--- exe/Node.hpp
+++ exe/Node.hpp
@@ -5,11 +5,16 @@
 #include "cpphop.h"
 #include "util.h"
 
-template <class State>
+template <class State,class Selector>
 class Node {
   public:
     State state;
     Tasks tasks;
     int depth;
     bTasks plan;
+    Selector selector;
+
+    friend bool operator== (Node<State,Selector> & lhs, Node<State,Selector> & rhs) {
+      return (lhs.state == rhs.state && lhs.tasks == rhs.tasks && lhs.depth == rhs.depth && lhs.plan == rhs.plan);
+    }
 };
--- exe/Tree.hpp
+++ exe/Tree.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/range/iterator_range.hpp>
 #include "Node.hpp"
 #include "util.h"
 
@@ -17,10 +18,38 @@ class TreeData {
 };
 
 
-template <class State>
+template <class State, class Selector>
 using Tree = boost::adjacency_list<boost::vecS,
                               boost::vecS,
                               boost::directedS,
-                              Node<State>,
+                              Node<State, Selector>,
                               boost::no_property,
                               TreeData>;
+
+template <class State, class Selector>
+auto get_successors(Tree<State,Selector> t,int i) {
+    return boost::make_iterator_range(boost::adjacent_vertices(i, t));
+}
+
+template <class State, class Selector>
+std::vector<int> get_successor_list(Tree<State,Selector> t,int i) {
+    std::vector<int> successors = {};
+    for (int successor : get_successors(t,i)) {
+      successors.push_back(successor);
+    }
+    return successors;
+}
+
+template <class State, class Selector>
+auto get_predecessors(Tree<State,Selector> t, int i) {
+  return boost::make_iterator_range(boost::inv_adjacent_vertices(i,t));
+}
+
+template <class State, class Selector>
+std::vector<int> get_predecessor_list(Tree<State,Selector> t,int i) {
+    std::vector<int> predecessors = {};
+    for (int predecessor : get_predecessors(t,i)) {
+      predecessors.push_back(predecessor);
+    }
+    return predecessors;
+}
