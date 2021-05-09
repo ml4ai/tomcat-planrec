e66dc3305c14fb2ead13b7090d329392053e1bd6
Loren Champlin <lorenchamplin@SBS-7185.local>
2021-05-08 17:29:53 -0700

    Added some functions for a MCTS algorithm

--- exe/cpphop.h
+++ exe/cpphop.h
@@ -128,8 +128,8 @@ bTasks seek_plan(State state,
     }
 }
 
-template <class State, class Domain>
-Tree<State> seek_planDFS(Tree<State> t,
+template <class State, class Domain, class Selector>
+Tree<State,Selector> seek_planDFS(Tree<State, Selector> t,
                  int v,
                  Domain domain) {
     std::cout << "depth: " << t[v].depth << std::endl;
@@ -149,7 +149,7 @@ Tree<State> seek_planDFS(Tree<State> t,
         Operator<State> op = domain.operators[task_id];
         std::optional<State> newstate = op(t[v].state, args);
         if (newstate) {
-            Node<State> n;
+            Node<State, Selector> n;
             n.state = newstate.value();
             n.tasks = t[v].tasks;
             n.tasks.pop_back();
@@ -168,7 +168,7 @@ Tree<State> seek_planDFS(Tree<State> t,
         for (auto method : relevant) {
             auto subtasks = method(t[v].state, args);
             if (subtasks.first) {
-                Node<State> n;
+                Node<State, Selector> n;
                 n.state = t[v].state;
                 n.tasks = t[v].tasks;
                 n.tasks.pop_back();
@@ -194,12 +194,13 @@ bTasks cpphop(State state,
     return result;
 }
 
-template <class State, class Domain>
+template <class State, class Domain, class Selector>
 Plans cpphopDFS(State state,
              Tasks tasks,
-             Domain domain) {
-    Tree<State> t;
-    Node<State> root;
+             Domain domain,
+             Selector selector) {
+    Tree<State,Selector> t;
+    Node<State,Selector> root;
     root.state = state;
     root.tasks = tasks;
     root.plan = {};
@@ -208,4 +209,136 @@ Plans cpphopDFS(State state,
     t = seek_planDFS(t,v,domain);
     print(t[boost::graph_bundle].plans);
     return t[boost::graph_bundle].plans;
+}
+
+template <class State, class Selector>
+std::pair<Tree<State, Selector>,int> selection(Tree<State,Selector> t, int v, double c, int r_l, int r_t) {
+  std::vector<int> successors = get_successor_list(t,v);
+  if (successors.empty()) {
+    return std::make_pair(t,v);
+  }
+  double max = 0.0;
+  int arg_max = successors.front();
+  for (int w : successors) {
+    if (t[w].selector.sims == 0) {
+      return selection(t,w,c,r_l,r_t);
+    }
+    double s = t[w].selector.selectFunc(t[v].selector.mean,c,r_l,r_t);
+    if (s > max) {
+      max = s;
+      arg_max = w;
+    }
+  }
+  return selection(t,arg_max,c,r_l,r_t);
+}
+
+template <class State, class Domain, class Selector>
+std::pair<Tree<State, Selector>, int> expansion(Tree<State,Selector> t, Domain domain, int v, int seed = 2021) {
+    Task task = t[v].tasks.back();
+    auto [task_id, args] = task;
+    
+    if (in(task_id, domain.operators)) {
+        Operator<State> op = domain.operators[task_id];
+        std::optional<State> newstate = op(t[v].state, args);
+        if (newstate) {
+            Node<State, Selector> n;
+            n.state = newstate.value();
+            n.tasks = t[v].tasks;
+            n.tasks.pop_back();
+            n.depth = t[v].depth + 1;
+            n.plan = t[v].plan;
+            n.plan.second.push_back(task);
+            int w = boost::add_vertex(n,t);
+            return std::make_pair(t,w);
+        }
+        throw std::logic_error("Action Preconditions failed during expansion!");
+    }
+
+    if (in(task_id, domain.methods)) {
+        auto relevant = domain.methods[task_id];
+        std::vector<int> c;
+        for (auto method : relevant) {
+            auto subtasks = method(t[v].state, args);
+            if (subtasks.first) {
+                Node<State, Selector> n;
+                n.state = t[v].state;
+                n.tasks = t[v].tasks;
+                n.tasks.pop_back();
+                n.depth = t[v].depth + 1;
+                for (auto i = subtasks.second.end();
+                     i != subtasks.second.begin();) {
+                    n.tasks.push_back(*(--i));
+                }
+                int w = boost::add_vertex(n,t);
+                c.push_back(w);
+            }
+        }
+        int r = *select_randomly(c.begin(),c.end(),seed);
+        return std::make_pair(t,r);
+    }
+    throw std::logic_error("No expansion possible!");
+ 
+}
+
+template <class State, class Domain, class Selector>
+double simulation(State state, Tasks tasks, Domain domain, Selector sr, int seed = 2021) {
+  if (tasks.size() == 0) {
+    return sr.rewardFunc(state);
+  }
+
+  Task task = tasks.back();
+  auto [task_id, args] = task;
+    
+  if (in(task_id, domain.operators)) {
+      Operator<State> op = domain.operators[task_id];
+      std::optional<State> newstate = op(state, args);
+      if (newstate) {
+          tasks.pop_back();
+          return simulation(newstate,tasks,domain,sr,seed);
+      }
+      throw std::logic_error("Action Preconditions failed during simulation!");
+  }
+
+  if (in(task_id, domain.methods)) {
+      auto relevant = domain.methods[task_id];
+      std::vector<Tasks> options;
+      for (auto method : relevant) {
+          auto subtasks = method(state, args);
+          if (subtasks.first) {
+              Tasks temp = tasks;
+              temp.pop_back();
+              for (auto i = subtasks.second.end();
+                    i != subtasks.second.begin();) {
+                  temp.push_back(*(--i));
+              }
+              options.push_back(temp);
+          }
+      }
+      Tasks r = *select_randomly(options.begin(),options.end(), seed);
+      return simulation(state,r,domain,sr,seed); 
+  }
+  throw std::logic_error("Simulation failed!");
+
+
+}
+
+template <class State, class Selector>
+std::pair<Tree<State, Selector>,int> backprop(Tree<State, Selector> t, int w) {
+  std::vector<int> successors = get_successor_list(t,w);
+  if (!(successors.empty())) {
+    t[w].selector.mean = 0;
+    t[w].selector.sims = t[w].selector.sims + 1;
+    for (int c : successors) {
+      t[w].selector.mean += t[c].selector.mean;
+    }
+    t[w].selector.mean /= successors.size();
+  }
+
+  std::vector<int> predecessors = get_predecessor_list(t,w);
+  if (predecessors.empty()) {
+    return std::make_pair(t,w);
+  }
+
+  return backprop(t,predecessors.back());
+    
 }
