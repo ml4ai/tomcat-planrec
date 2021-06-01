4810a24d98c203ae0acf95154bba1bbb85f5b37a
Adarsh Pyarelal <adarsh@arizona.edu>
2021-06-01 08:49:19 -0700

    Switching to processing file

--- apps/parser/parser.cpp
+++ apps/parser/parser.cpp
@@ -1,13 +1,14 @@
 #include <boost/config/warning_disable.hpp>
-#include <boost/spirit/home/x3/support/ast/position_tagged.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/deque.hpp>
 #include <boost/fusion/include/io.hpp>
 #include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/position_tagged.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
-#include <boost/fusion/include/deque.hpp>
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 namespace client {
     namespace ast {
@@ -45,7 +46,8 @@ namespace client {
 
 BOOST_FUSION_ADAPT_STRUCT(client::ast::Entity, name, type)
 BOOST_FUSION_ADAPT_STRUCT(client::ast::Action, name, parameters)
-BOOST_FUSION_ADAPT_STRUCT(client::ast::Domain, name, requirements, types, actions)
+BOOST_FUSION_ADAPT_STRUCT(
+    client::ast::Domain, name, requirements, types, actions)
 
 namespace client {
     ///////////////////////////////////////////////////////////////////////////////
@@ -54,26 +56,26 @@ namespace client {
     namespace parser {
         namespace x3 = boost::spirit::x3;
 
-        using x3::ascii::char_, x3::lexeme, x3::lit, x3::alnum, x3::_attr,
-              x3::_val;
         using boost::fusion::at_c;
         using client::ast::Entity;
+        using x3::ascii::char_, x3::lexeme, x3::lit, x3::alnum, x3::_attr,
+            x3::_val, x3::space, x3::eol;
 
-        auto const name = lexeme[+alnum];
+        auto const name = lexeme[+(char_ - '?' - '(' - ')' - ':' - space)];
         auto const requirement = ':' >> name;
         auto const variable = '?' >> name;
-        auto const require_def = '(' >> lit(":requirements") >> +requirement >> ')';
+        auto const require_def = '(' >> lit(":requirements") >> +requirement >>
+                                 ')';
         auto const types_def = '(' >> lit(":types") >> +name >> ')';
 
+        x3::rule<class TTypedList, ast::TypedList> const typed_list =
+            "typed_list";
 
-        x3::rule<class TTypedList, ast::TypedList> const typed_list = "typed_list";
-
-        auto add_implicitly_typed_entity = [](auto& ctx){
+        auto add_implicitly_typed_entity = [](auto& ctx) {
             _val(ctx).push_back(Entity(_attr(ctx)));
         };
 
-        auto add_explicitly_typed_entities = [](auto& ctx){
-
+        auto add_explicitly_typed_entities = [](auto& ctx) {
             auto dq = _attr(ctx);
             std::vector<std::string> names = at_c<0>(dq);
             std::string type = at_c<1>(dq);
@@ -84,29 +86,26 @@ namespace client {
             }
         };
 
-
-        auto const implicitly_typed_list = *variable[add_implicitly_typed_entity];
-        auto const explicitly_typed_list = (+variable >> '-' >> name)[add_explicitly_typed_entities];
-        auto const typed_list_def = *explicitly_typed_list >> -implicitly_typed_list;
+        auto const implicitly_typed_list =
+            *variable[add_implicitly_typed_entity];
+        auto const explicitly_typed_list =
+            (+variable >> '-' >> name)[add_explicitly_typed_entities];
+        auto const typed_list_def =
+            *explicitly_typed_list >> -implicitly_typed_list;
 
         x3::rule<class TAction, ast::Action> const action = "action";
-        auto const action_def = 
-            '('
-            >> lit(":action")
-            >> name
-            >> '(' >> lit(":parameters") >> '(' >> typed_list >> ')' >> ')'
-            >> ')';
+        auto const action_def = '(' >> lit(":action") >> name >>
+                                lit(":parameters") >> '(' >> typed_list
+                                >> ')' >> ')';
 
         x3::rule<class TDomain, ast::Domain> const domain = "domain";
-        auto const domain_def = 
-            '(' 
-            >> lit("define") >> '(' >> lit("domain") >> name >> ')' 
-            >> -require_def 
-            >> -types_def 
-            >> +action
-            >> ')';
+        auto const domain_def = '(' >> lit("define") >> '(' >>
+                                lit("domain") >> name >> ')' >> -require_def >>
+                                -types_def >> +action >> ')';
 
         BOOST_SPIRIT_DEFINE(typed_list, action, domain);
+
+        auto const skipper = space | lexeme[';' >> *(char_ - eol) >> eol];
 
     } // namespace parser
 } // namespace client
@@ -115,18 +114,18 @@ void print(client::ast::Domain dom) {
     using namespace std;
     cout << "Name: " << dom.name << endl;
     cout << "Requirements: " << endl;
-    for (auto x: dom.requirements) {
-        cout << '"' << x  << '"' << endl;
+    for (auto x : dom.requirements) {
+        cout << '"' << x << '"' << endl;
     }
     cout << endl;
     cout << "Types: " << endl;
-    for (auto x: dom.types) {
+    for (auto x : dom.types) {
         cout << x;
     }
     cout << endl;
 
     cout << "Actions:" << endl;
-    for (auto x: dom.actions) {
+    for (auto x : dom.actions) {
         cout << x.name << endl;
         cout << "parameters: " << endl;
         for (auto p : x.parameters) {
@@ -140,35 +139,54 @@ void print(client::ast::Domain dom) {
 ////////////////////////////////////////////////////////////////////////////
 //  Main program
 ////////////////////////////////////////////////////////////////////////////
-int main() {
+int main(int argc, char* argv[]) {
     using namespace std;
-    using boost::spirit::x3::ascii::space;
     typedef string::const_iterator iterator_type;
     using client::parser::domain;
 
-    string str;
-    while (getline(cin, str)) {
-        if (str.empty() || str[0] == 'q' || str[0] == 'Q')
-            break;
+    char const* filename;
+    if (argc > 1) {
+        filename = argv[1];
+    }
+    else {
+        std::cerr << "Error: No input file provided." << std::endl;
+        return 1;
+    }
 
-        client::ast::Domain dom;
-        iterator_type iter = str.begin();
-        iterator_type const end = str.end();
-        bool r = phrase_parse(iter, end, domain, space, dom);
+    ifstream in(filename, ios_base::in);
 
-        if (r && iter == end) {
-            //cout << boost::fusion::tuple_open('[');
-            //cout << boost::fusion::tuple_close(']');
-            //cout << boost::fusion::tuple_delimiter(", ");
+    if (!in) {
+        cerr << "Error: Could not open input file: " << filename
+                  << endl;
+        return 1;
+    }
 
-            cout << "-------------------------\n";
-            cout << "Parsing succeeded\n";
-            print(dom);
-            cout << "\n-------------------------\n";
-        }
-        else {
-            cout << "Parsing failed\n";
-        }
+    string storage;         // We will read the contents here.
+    in.unsetf(ios::skipws); // No white space skipping!
+    copy(istream_iterator<char>(in),
+              istream_iterator<char>(),
+              back_inserter(storage));
+
+
+    string::const_iterator iter = storage.begin();
+    string::const_iterator end = storage.end();
+    client::ast::Domain dom;
+    bool r = phrase_parse(iter, end, domain, client::parser::skipper, dom);
+
+    if (r && iter == end)
+    {
+        std::cout << "-------------------------\n";
+        std::cout << "Parsing succeeded\n";
+        std::cout << "-------------------------\n";
+        print(dom);
+        return 0;
+    }
+    else
+    {
+        std::cout << "-------------------------\n";
+        std::cout << "Parsing failed\n";
+        std::cout << "-------------------------\n";
+        return 1;
     }
     return 0;
 }
---
+++ apps/parser/test.pddl
@@ -0,0 +1,7 @@
+; Example domain for testing
+
+(define 
+  (domain construction)
+  (:requirements :strips :typing)
+  (:action build-wall 
+   :parameters (?s - site ?b - bricks)))
