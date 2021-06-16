eb5da4756ba08a27e966d0fa08b00d8ae7eab5ce
Adarsh Pyarelal <adarsh@arizona.edu>
2021-06-15 17:04:05 -0700

    Improvements in tests

--- test/test_unification.cpp
+++ test/test_unification.cpp
@@ -1,34 +1,36 @@
-#include "unification.h"
+#define BOOST_TEST_MODULE TestUnification
+
+#include <boost/test/included/unit_test.hpp>
+
+//#include "unification.h"
 
 using namespace std;
 
-int main() {
+BOOST_AUTO_TEST_CASE(test_unification) {
 
     // sample predicates for testing unification
-    typedef boost::variant<client::ast::Entity, client::ast::Variable, Predicate> argument; // only boost::variant allows for recursion
-    typedef vector<argument> v_args;
+    //typedef boost::variant<client::ast::Entity, client::ast::Variable, Predicate> argument; // only boost::variant allows for recursion
+    //typedef vector<argument> v_args;
 
-    client::ast::Entity John("John");
+    //client::ast::Entity John("John");
+    //client::ast::Entity Richard("Richard");
+    //client::ast::Variable X("X");
 
-    client::ast::Entity Richard("Richard");
+    //argument J1=John;
+    //argument R1=Richard;
+    //argument X1=X;
 
-    client::ast::Variable X("X");
+    //v_args v1 (J1, R1);
+    //v_args v2 (J1, X1);
 
-    argument J1=John;
-    argument R1=Richard;
-    argument X1=X;
+    //KnowledgeBase::Predicate P1("Knows", v1);
+    //KnowledgeBase::Predicate P2("Knows", v2);
 
-    v_args v1 (J1, R1);
-    v_args v2 (J1, X1);
+    //// setting up to run unification 
+    //typedef unordered_map<string, string> subs;
+    //subs s1;
 
-    KnowledgeBase::Predicate P1("Knows", v1);
-    KnowledgeBase::Predicate P2("Knows", v2);
+    //s1 = KnowledgeBase.unification(P1, P2, s1);
 
-    // setting up to run unification 
-    typedef unordered_map<string, string> subs;
-    subs s1;
-
-    s1 = KnowledgeBase.unification(P1, P2, s1);
-
-    cout << s1;
+    //cout << s1;
 }
