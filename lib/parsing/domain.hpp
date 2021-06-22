59404c92ee5bc1dd8432980911cc0fa92489d86a
Adarsh Pyarelal <adarsh@email.arizona.edu>
2021-06-21 18:49:33 -0700

    minor edits

--- lib/parsing/domain.hpp
+++ lib/parsing/domain.hpp
@@ -10,13 +10,13 @@ namespace client {
     ///////////////////////////////////////////////////////////////////////////////
     namespace parser {
         namespace x3 = boost::spirit::x3;
-        using x3::space, x3::lexeme, x3::char_, x3::eol;
+        using x3::space, x3::lexeme, x3::char_, x3::eol, x3::rule;
         static auto const skipper = space | lexeme[';' >> *(char_ - eol) >> eol];
         // Set up the skip parser so that it can be used from parser.cpp
         using skipper_type=decltype(skipper);
         using phrase_context_type = x3::phrase_parse_context<skipper_type>::type;
-        using domain_type = x3::rule<class TDomain, ast::Domain>;
-        using problem_type = x3::rule<class TProblem, ast::Problem>;
+        using domain_type = rule<class TDomain, ast::Domain>;
+        using problem_type = rule<class TProblem, ast::Problem>;
 
         BOOST_SPIRIT_DECLARE(domain_type);
         BOOST_SPIRIT_DECLARE(problem_type);
