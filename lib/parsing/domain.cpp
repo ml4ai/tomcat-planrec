4f4a10c24bccd3eab8f6c25695af801dacba53fe
Adarsh Pyarelal <adarsh@arizona.edu>
2021-06-22 17:07:42 -0700

    Adding constants and requirements
    WIP, not compiling yet
    
    wip

--- lib/parsing/domain.cpp
+++ lib/parsing/domain.cpp
@@ -7,8 +7,8 @@ namespace parser {
     BOOST_SPIRIT_INSTANTIATE(primitive_type_type, iterator_type, context_type);
     BOOST_SPIRIT_INSTANTIATE(either_type_type, iterator_type, context_type);
     BOOST_SPIRIT_INSTANTIATE(type_type, iterator_type, context_type);
-    BOOST_SPIRIT_INSTANTIATE(implicitly_typed_list_type, iterator_type, context_type);
-    BOOST_SPIRIT_INSTANTIATE(explicitly_typed_list_type, iterator_type, context_type);
-    BOOST_SPIRIT_INSTANTIATE(typed_list_type, iterator_type, context_type);
+    BOOST_SPIRIT_INSTANTIATE(typed_list_names_type, iterator_type, context_type);
+    BOOST_SPIRIT_INSTANTIATE(typed_list_variables_type, iterator_type, context_type);
+    BOOST_SPIRIT_INSTANTIATE(atomic_formula_skeleton_type, iterator_type, context_type);
     BOOST_SPIRIT_INSTANTIATE(domain_type, iterator_type, context_type);
 } // namespace parser
--- lib/parsing/domain_def.hpp
+++ lib/parsing/domain_def.hpp
@@ -11,7 +11,7 @@
 namespace parser {
     using ast::Constant, ast::Variable, ast::PrimitiveType, ast::EitherType,
         ast::Type, ast::ImplicitlyTypedList, ast::ExplicitlyTypedList,
-        ast::TypedList, ast::Name;
+        ast::TypedList, ast::Name, ast::AtomicFormulaSkeleton;
 
     using boost::fusion::at_c;
     using x3::lexeme, x3::lit, x3::alnum, x3::_attr, x3::_val, x3::space,
@@ -27,10 +27,10 @@ namespace parser {
         "primitive_type";
     rule<class TEitherType, EitherType> const either_type = "either_type";
     rule<class TType, Type> const type = "type";
-    rule<class TExplicitlyTypedList, ExplicitlyTypedList<Name>> const
-        explicitly_typed_list = "explicitly_typed_list";
-    rule<class TImplicitlyTypedList, ImplicitlyTypedList<Name>> const
-        implicitly_typed_list = "implicitly_typed_list";
+    //rule<class TExplicitlyTypedList, ExplicitlyTypedList<Name>> const
+        //explicitly_typed_list = "explicitly_typed_list";
+    //rule<class TImplicitlyTypedList, ImplicitlyTypedList<Name>> const
+        //implicitly_typed_list = "implicitly_typed_list";
     rule<class TTypedList, TypedList<Name>> const typed_list = "typed_list";
     rule<class TTypes, TypedList<Name>> const types = "types";
     rule<class Predicate, Name> const predicate = "predicate";
@@ -40,26 +40,58 @@ namespace parser {
     rule<class TRequirement, std::vector<Name>> const requirement =
         "requirement";
 
+    rule<class TConstants, TypedList<Name>> const constants = "constants";
+    rule<class TAtomicFormulaSkeleton, AtomicFormulaSkeleton> const
+        atomic_formula_skeleton = "atomic_formula_skeleton";
+
     auto const requirement_def = ':' >> name;
     auto const predicate_def = name;
-    auto const requirements_def = '(' >> lit(":requirements") >> +requirement >> ')';
+    auto const requirements_def = '(' >> lit(":requirements") >> +requirement >>
+                                  ')';
     auto const constant_def = name;
     auto const variable_def = '?' >> name;
     auto const primitive_type_def = name;
     auto const either_type_def = '(' >> lit("either") >> +primitive_type >> ')';
     auto const type_def = primitive_type | either_type;
 
-    auto const explicitly_typed_list_def = +name >> '-' >> type;
+    template <class T> auto typed_list_parser(T parser) {
+        auto const implicitly_typed_list = *parser;
+        auto const explicitly_typed_list = (+parser >> '-' >> name);
+        auto const typed_list_def =
+            *explicitly_typed_list >> -implicitly_typed_list;
+        return typed_list_def;
+    }
 
-    auto const implicitly_typed_list_def = *name;
+    rule<class TTypedListNames, TypedList<Name>> const typed_list_names = "typed_list_names";
+    rule<class TTypedListVariables, TypedList<Variable>> const typed_list_variables = "typed_list_variables";
 
-    auto const typed_list_def =
-        *explicitly_typed_list >> -implicitly_typed_list;
+    auto const typed_list_names_def = typed_list_parser(name);
+    auto const typed_list_variables_def = typed_list_parser(variable_def);
 
-    auto const types_def = '(' >> lit(":types") >> typed_list >> ')';
+    template <class RuleID, class U, class Parser>
+    auto typed_list_rule(Parser parser) {
+        return rule<RuleID, TypedList<U>>{"typed_list"} =
+                   typed_list_parser<U>(parser);
+    }
+
+    //auto const typed_list_names = typed_list_rule<class Names, ast::Name>(name);
+    //auto const typed_list_variables = typed_list_rule<class Variables, ast::Variable>(variable);
+
+    //auto const explicitly_typed_list_def = +name >> '-' >> type;
+    //auto const implicitly_typed_list_def = *name;
+    //auto const typed_list_def = typed_list_names;
+
+    auto const types_def = '(' >> lit(":types") >> typed_list_names >> ')';
+    auto const constants_def = '(' >> lit(":constants") >> typed_list_names >> ')';
+
+    auto const atomic_formula_skeleton_def =
+        '(' >> predicate >> typed_list_variables >> ')';
     auto const domain_def = '(' >> lit("define") >> '(' >> lit("domain") >> name
-                            >> ')' >> requirements >> -types >> ')';
+                            >> ')' >> -requirements >> -types >> -constants >>
+                            ')';
 
+    BOOST_SPIRIT_DEFINE(typed_list_names);
+    BOOST_SPIRIT_DEFINE(typed_list_variables);
     BOOST_SPIRIT_DEFINE(constant,
                         variable,
                         primitive_type,
@@ -67,12 +99,12 @@ namespace parser {
                         type,
                         types,
                         predicate,
-                        explicitly_typed_list,
-                        implicitly_typed_list,
-                        typed_list,
                         domain,
                         requirement,
-                        requirements);
+                        constants,
+                        atomic_formula_skeleton,
+                        requirements
+                        );
 
 } // namespace parser
 
@@ -81,6 +113,10 @@ parser::variable_type variable() { return parser::variable; }
 parser::primitive_type_type primitive_type() { return parser::primitive_type; }
 parser::either_type_type either_type() { return parser::either_type; }
 parser::type_type type() { return parser::type; }
-parser::typed_list_type typed_list() { return parser::typed_list; }
+parser::typed_list_names_type typed_list_names() { return parser::typed_list_names; }
+parser::typed_list_variables_type typed_list_variables() { return parser::typed_list_variables; }
 parser::domain_type domain() { return parser::domain; }
 parser::requirements_type requirements() { return parser::requirements; }
+parser::atomic_formula_skeleton_type atomic_formula_skeleton() {
+    return parser::atomic_formula_skeleton;
+}
