c1c81c087b601868b0c1997d66e2c2465f82300f
Adarsh Pyarelal <adarsh@arizona.edu>
2021-06-28 15:42:03 -0700

    Added test stub for CNF conversion

---
+++ lib/CNF.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "parsing/ast.hpp"
+
+using ast::Sentence;
---
+++ test/test_cnf_conversion.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE TestUnification
+
+#include <boost/test/included/unit_test.hpp>
+
+#include "CNF.h"
+#include <iostream>
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(test_cnf_conversion) {}
