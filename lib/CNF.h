7a09e93744e09ac21972b6a33349eb2fe03fdd5f
Adarsh Pyarelal <adarsh@arizona.edu>
2021-06-28 17:22:00 -0700

    simple visitor example

--- lib/CNF.h
+++ lib/CNF.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include "Clause.h"
+#include "boost/variant.hpp"
 #include "parsing/ast.hpp"
 
-using ast::Sentence;
+namespace ast {
+    class DistributeOrOverAnd : public boost::static_visitor<ast::Sentence> {
+      public:
+        Nil operator()(Nil s) const { return s; }
+        AtomicFormula<Term> operator()(AtomicFormula<Term> s) const {
+            return s;
+        }
+        Literal<Term> operator()(Literal<Term> s) const { return s; }
+        AndSentence operator()(AndSentence s) const { return s; }
+        OrSentence operator()(OrSentence s) const { return s; }
+        NotSentence operator()(NotSentence s) const { return s; }
+        ImplySentence operator()(ImplySentence s) const { return s; }
+        ExistsSentence operator()(ExistsSentence s) const { return s; }
+        ForallSentence operator()(ForallSentence s) const { return s; }
+    };
+} // namespace ast
--- test/test_cnf_conversion.cpp
+++ test/test_cnf_conversion.cpp
@@ -3,8 +3,13 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "CNF.h"
+#include "parsing/parse.hpp"
+#include "parsing/domain.hpp"
 #include <iostream>
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(test_cnf_conversion) {}
+BOOST_AUTO_TEST_CASE(test_cnf_conversion) {
+    auto s = parse<ast::Sentence>("(and (a) (or b c))", sentence());
+    auto clauses = boost::apply_visitor(ast::DistributeOrOverAnd(), s);
+}
