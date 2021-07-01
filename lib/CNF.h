1bc37d08e1f4c7590064a265c4912ec33b283622
ualiangzhang <liangzh@email.arizona.edu>
2021-07-01 15:00:20 -0700

    implementing CNF converter

--- lib/CNF.h
+++ lib/CNF.h
@@ -3,17 +3,33 @@
 #include "Clause.h"
 #include "boost/variant.hpp"
 #include "parsing/ast.hpp"
+#include <typeinfo>
+#include <iostream>
+
+using boost::get;
+using namespace std;
 
 namespace ast {
+    std::vector<Clause> to_CNF(Sentence s);
     struct DistributeOrOverAnd : public boost::static_visitor<Sentence> {
 
         std::vector<Clause> clauses = {};
         Sentence operator()(Nil s) const { return s; }
-        Sentence operator()(AtomicFormula<Term> s) const { return s; }
+        //        Sentence operator()(AtomicFormula<Term> s) const { return s; }
         Sentence operator()(Literal<Term> s) const { return s; }
         Sentence operator()(AndSentence s) const { return s; }
         Sentence operator()(OrSentence s) const {
-            // auto
+
+            auto os = get<OrSentence>(s);
+            auto first = os.sentences[0];
+            auto second = os.sentences[1];
+
+            std::cout << (typeid(second) == typeid(AndSentence)) << endl;
+            std::cout << (typeid(get<AndSentence>(first)) == typeid(AndSentence)) << endl;
+            cout << endl;
+
+
+
             return s;
         }
         Sentence operator()(NotSentence s) const { return s; }
--- test/test_cnf_conversion.cpp
+++ test/test_cnf_conversion.cpp
@@ -1,21 +1,42 @@
-#define BOOST_TEST_MODULE TestUnification
+#define BOOST_TEST_MODULE TestCNF
+
+
+#include "CNF.h"
 
 #include <boost/test/included/unit_test.hpp>
 
-#include "CNF.h"
-#include "parsing/parse.hpp"
-#include "parsing/domain.hpp"
+#include <boost/variant/get.hpp>
+#include <fstream>
 #include <iostream>
+#include <string>
 
+#include "parsing/ast.hpp"
+#include "parsing/ast_adapted.hpp"
+#include "parsing/domain.hpp"
+#include "parsing/parse.hpp"
+#include "util.h"
+#include "Clause.h"
+#include <boost/optional.hpp>
+#include <boost/spirit/home/x3/support/ast/variant.hpp>
+
+using boost::unit_test::framework::master_test_suite;
+namespace x3 = boost::spirit::x3;
 using namespace std;
+using namespace ast;
 using boost::get;
 
 BOOST_AUTO_TEST_CASE(test_cnf_conversion) {
-    auto s1 = parse<ast::Sentence>("(predicate c1 c2)", sentence());
-    auto s = parse<ast::Sentence>("(or (a) (and b c))", sentence());
+    //  test imply
+    auto s1 =
+        parse<Sentence>("(imply (Animal ?y) (Loves ?x ?y))", sentence());
+
+//    auto s1 = parse<ast::Sentence>("(predicate c1 c2)", sentence());
+    auto s2 = parse<Sentence>("(or (a) (and (b) (c)))", sentence());
     // Should produce clauses: (a ∨ b), (a ∨ c)
-    auto clauses = to_CNF(s);
+
+    auto clauses = to_CNF(s2);
+    cout << endl;
 
     // THIS TEST FAILS, FIXME
-    //BOOST_TEST(get<ast::AtomicFormula<ast::Term>>(clauses[0].literals[0]).predicate.name == "a");
+//    BOOST_TEST(get<ast::AtomicFormula<ast::Term>>(clauses[0].literals[0]).predicate.name == "a");
 }
