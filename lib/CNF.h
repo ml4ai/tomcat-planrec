a0db494717ed3d5359a77c0de6dfe05f103db356
ualiangzhang <liangzh@email.arizona.edu>
2021-07-05 18:12:11 -0700

    add the pair converter of sentences

--- lib/CNF.h
+++ lib/CNF.h
@@ -5,17 +5,51 @@
 #include "parsing/ast.hpp"
 #include <typeinfo>
 #include <iostream>
+#include <typeinfo>
 
 using boost::get;
 using namespace std;
 
 namespace ast {
+    struct GeneratePairSentence : public boost::static_visitor<Sentence> {
+        Sentence operator()(Nil s) const { return s; }
+        Sentence operator()(Literal<Term> s) const { return s; }
+        Sentence operator()(ConnectedSentence s) const {
+            auto rs = ConnectedSentence();
+            rs.connector = (s.connector == "and") ? "and" : "or";
+            if (s.sentences.size() <= 2){
+                for (auto sentence : s.sentences) {
+                    rs.sentences.push_back(boost::apply_visitor(
+                        GeneratePairSentence(), Sentence(sentence)));
+                }
+                return rs;
+            }
+
+            auto last_sen = s.sentences.back();
+            s.sentences.pop_back();
+            rs.sentences.push_back(boost::apply_visitor(GeneratePairSentence(), (Sentence)s));
+            rs.sentences.push_back(boost::apply_visitor(GeneratePairSentence(), (Sentence)last_sen));
+            return rs;
+        }
+        Sentence operator()(NotSentence s) const { return s; }
+        Sentence operator()(ImplySentence s) const { return s; }
+        Sentence operator()(ExistsSentence s) const { return s; }
+        Sentence operator()(ForallSentence s) const { return s; }
+    };
+
     struct DistributeOrOverAnd : public boost::static_visitor<Sentence> {
 
         std::vector<Clause> clauses = {};
         Sentence operator()(Nil s) const { return s; }
         Sentence operator()(Literal<Term> s) const { return s; }
-        Sentence operator()(ConnectedSentence s) const { return s; }
+        Sentence operator()(ConnectedSentence s) const {
+//            auto a = s.sentences[0];
+//            auto b = s.sentences[1].connector;
+//            cout << get<ConnectedSentence>(s.sentences[0]).connector;
+//            cout << get<ConnectedSentence>(s.sentences[1]).connector;
+
+
+            return s; }
         Sentence operator()(NotSentence s) const { return s; }
         Sentence operator()(ImplySentence s) const { return s; }
         Sentence operator()(ExistsSentence s) const { return s; }
@@ -23,7 +57,11 @@ namespace ast {
     };
 
     Sentence to_CNF(Sentence s) {
-        auto visitor = DistributeOrOverAnd();
-        return boost::apply_visitor(visitor, s);
+//        auto visitor1 = GeneratePairSentence();
+        auto s1 = boost::apply_visitor(GeneratePairSentence(), s);
+//        auto visitor2 = DistributeOrOverAnd();
+//        auto s2 = get<ConnectedSentence>(s1);
+//        auto s3 = get<ConnectedSentence>(get<ConnectedSentence>(s2.sentences[1]).sentences[0]);
+        return boost::apply_visitor(DistributeOrOverAnd(), s1);
     }
 } // namespace ast
--- test/test_cnf_conversion.cpp
+++ test/test_cnf_conversion.cpp
@@ -26,16 +26,66 @@ using namespace ast;
 using boost::get;
 
 BOOST_AUTO_TEST_CASE(test_cnf_conversion) {
+    // test the arguments conversion
+    //  (or (b) (c) (d)) => (or (or (b) (c)) (d))
+    auto a1 = parse<Sentence>("(or (b) (c) (d))", sentence());
+    auto a2 = boost::apply_visitor(GeneratePairSentence(), a1);
+    auto a3 = get<ConnectedSentence>(a2);
+    BOOST_TEST(a3.connector == "or");
+    auto a4 = get<ConnectedSentence>(a3.sentences[0]);
+    auto a5 = get<Literal<Term>>(a4.sentences[0]);
+    BOOST_TEST(a5.predicate == "b");
+    auto a6 = get<Literal<Term>>(a4.sentences[1]);
+    BOOST_TEST(a6.predicate == "c");
+    auto a7 = get<Literal<Term>>(a3.sentences[1]);
+    BOOST_TEST(a7.predicate == "d");
+
+    //  (and (b) (c) (d)) => (and (and (b) (c)) (d))
+    auto b1 = parse<Sentence>("(and (b) (c) (d))", sentence());
+    auto b2 = boost::apply_visitor(GeneratePairSentence(), b1);
+    auto b3 = get<ConnectedSentence>(b2);
+    BOOST_TEST(b3.connector == "and");
+    auto b4 = get<ConnectedSentence>(b3.sentences[0]);
+    auto b5 = get<Literal<Term>>(b4.sentences[0]);
+    BOOST_TEST(b5.predicate == "b");
+    auto b6 = get<Literal<Term>>(b4.sentences[1]);
+    BOOST_TEST(b6.predicate == "c");
+    auto b7 = get<Literal<Term>>(b3.sentences[1]);
+    BOOST_TEST(b7.predicate == "d");
+
+    //  (or (a) (b) (and (c) (d) (e))) => (or (or (a) (b)) (and (and (c) (d)) (e)))
+    auto c1 = parse<Sentence>("(or (a) (b) (and (c) (d) (e)))", sentence());
+    auto c2 = boost::apply_visitor(GeneratePairSentence(), c1);
+    auto c3 = get<ConnectedSentence>(c2);
+    BOOST_TEST(c3.connector == "or");
+    auto c4 = get<ConnectedSentence>(c3.sentences[0]);
+    BOOST_TEST(c4.connector == "or");
+    auto c5 = get<Literal<Term>>(c4.sentences[0]);
+    BOOST_TEST(c5.predicate == "a");
+    auto c6 = get<Literal<Term>>(c4.sentences[1]);
+    BOOST_TEST(c6.predicate == "b");
+    auto c7 = get<ConnectedSentence>(c3.sentences[1]);
+    BOOST_TEST(c7.connector == "and");
+    auto c8 = get<ConnectedSentence>(c7.sentences[0]);
+    auto c9 = get<Literal<Term>>(c8.sentences[0]);
+    BOOST_TEST(c9.predicate == "c");
+    auto c10 = get<Literal<Term>>(c7.sentences[1]);
+    BOOST_TEST(c10.predicate == "e");
+
+
+
+
+
     //  test imply
-    auto s1 =
-        parse<Sentence>("(imply (Animal ?y) (Loves ?x ?y))", sentence());
+//    auto s1 =
+//        parse<Sentence>("(imply (Animal ?y) (Loves ?x ?y))", sentence());
 
 //    auto s1 = parse<ast::Sentence>("(predicate c1 c2)", sentence());
-    auto s2 = parse<Sentence>("(or (a) (and (b) (c)))", sentence());
-
-    // Should produce (a ∨ b) ∧ (a ∨ c)
-    auto clauses = to_CNF(s2);
-    cout << endl;
+//    auto s2 = parse<Sentence>("(or (a) (and (b) (c) (d)))", sentence());
+//
+//    // Should produce (a ∨ b) ∧ (a ∨ c)
+//    auto clauses = to_CNF(s2);
+//    cout << endl;
 
     // THIS TEST FAILS, FIXME
 //    BOOST_TEST(get<ast::AtomicFormula<ast::Term>>(clauses[0].literals[0]).predicate.name == "a");
