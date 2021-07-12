da2cbea07c4456c3d6e5b6df1304bdc867ac8156
ualiangzhang <liangzh@email.arizona.edu>
2021-07-12 11:01:25 -0700

    add StandardizeQuantiferVariables (unfinished)

--- lib/CNF.h
+++ lib/CNF.h
@@ -5,11 +5,20 @@
 #include "parsing/ast.hpp"
 #include <iostream>
 #include <typeinfo>
+#include <map>
 
 using boost::get;
 using namespace std;
 
 namespace ast {
+    bool contains(auto v, auto x) {
+        if(std::find(v.begin(), v.end(), x) != v.end()) {
+            return true;
+        } else {
+            return false;
+        }
+      }
+
     struct GetSentenceType : public boost::static_visitor<std::string> {
         std::string operator()(Nil s) const { return "Nil"; }
         std::string operator()(Literal<Term> s) const { return "Literal"; }
@@ -120,9 +129,8 @@ namespace ast {
             if (boost::apply_visitor(GetSentenceType(), (Sentence)s1) ==
                 "NotSentence") {
                 auto s_ = get<NotSentence>(s1);
-                return boost::apply_visitor(
-                    NegationsIn(),
-                    (Sentence)s_.sentence);
+                return boost::apply_visitor(NegationsIn(),
+                                            (Sentence)s_.sentence);
             }
 
             if (boost::apply_visitor(GetSentenceType(), (Sentence)s1) ==
@@ -183,8 +191,7 @@ namespace ast {
             }
 
             NotSentence rs;
-            rs.sentence =
-                boost::apply_visitor(NegationsIn(), (Sentence)s1);
+            rs.sentence = boost::apply_visitor(NegationsIn(), (Sentence)s1);
 
             return rs;
         }
@@ -213,6 +220,91 @@ namespace ast {
                 boost::apply_visitor(NegationsIn(), (Sentence)s.sentence);
             return rs;
         }
+    };
+
+    struct StandardizeApartIndexical {
+        int index = 0;
+        std::string getPrefix() {
+            return "q";
+        }
+        int getNextIndex() {
+            return this->index++;
+        }
+    };
+
+    struct StandardizeQuantiferVariables
+        : public boost::static_visitor<Sentence> {
+        ast::StandardizeApartIndexical quantifiedIndexical;
+        int i = quantifiedIndexical.getNextIndex();
+
+        Sentence operator()(Nil s) const { return s; }
+        Sentence operator()(Literal<Term> s) const { return s; }
+        Sentence operator()(ConnectedSentence s) const {
+            auto s1 =
+                boost::apply_visitor(StandardizeQuantiferVariables(), (Sentence)s.sentences[0]);
+            auto s2 =
+                boost::apply_visitor(StandardizeQuantiferVariables(), (Sentence)s.sentences[1]);
+            ConnectedSentence rs;
+            rs.connector = s.connector;
+            rs.sentences.push_back(s1);
+            rs.sentences.push_back(s2);
+            return rs;
+        }
+        Sentence operator()(NotSentence s) const {
+            NotSentence rs;
+            rs.sentence = boost::apply_visitor(StandardizeQuantiferVariables(), (Sentence)s.sentence);
+            return rs;
+        }
+
+        Sentence operator()(ImplySentence s) const { return s; }
+        // can't be constant
+        Sentence operator()(ExistsSentence s, vector<Variable> arg) {
+            vector<Variable> seenSoFar = arg;
+            std::map<Variable, Term> localSubst;
+            std::vector<Variable> replVariables;
+            for (auto & v : s.variables.implicitly_typed_list.value()) {
+                if (contains(seenSoFar, v)) {
+                    Variable sV;
+                    sV.name = this->quantifiedIndexical.getPrefix()
+                              + std::to_string(this->quantifiedIndexical.getNextIndex());
+                    localSubst.insert(std::pair<Variable, Term>(v, sV));
+                    // Replacement variables should contain new name for variable
+                    replVariables.push_back(sV);
+                } else {
+                    // Not already replaced, this name is good
+                    replVariables.push_back(v);
+                }
+            }
+            // Apply the local subst
+            Sentence subst = substVisitor.subst(localSubst,
+                                                sentence.getQuantified());
+
+            // Ensure all my existing and replaced variable
+            // names are tracked
+            seenSoFar.addAll(replVariables);
+
+            Sentence sQuantified = (Sentence) subst.accept(this, arg);
+
+            ForallSentence rs;
+            rs.variables. = replVariables;
+            rs
+
+            return new QuantifiedSentence(sentence.getQuantifier(), replVariables,
+                                          sQuantified);
+        }
+        Sentence operator()(ForallSentence s) const { return s; }
+    };
+
+    struct RemoveQuantifiers
+        : public boost::static_visitor<Sentence> {
+        Sentence operator()(Nil s) const { return s; }
+        Sentence operator()(Literal<Term> s) const { return s; }
+        Sentence operator()(ConnectedSentence s) const { return s; }
+        Sentence operator()(NotSentence s) const { return s; }
+
+        Sentence operator()(ImplySentence s) const { return s; }
+        Sentence operator()(ExistsSentence s) const { return s; }
+        Sentence operator()(ForallSentence s) const { return s; }
     };
 
     struct DistributeOrOverAnd : public boost::static_visitor<Sentence> {
@@ -282,6 +374,18 @@ namespace ast {
             return rs;
         }
         Sentence operator()(NotSentence s) const { return s; }
+        Sentence operator()(ImplySentence s) const { return s; }
+        Sentence operator()(ExistsSentence s) const { return s; }
+        Sentence operator()(ForallSentence s) const { return s; }
+    };
+
+    struct CNFConstructor
+        : public boost::static_visitor<Sentence> {
+        Sentence operator()(Nil s) const { return s; }
+        Sentence operator()(Literal<Term> s) const { return s; }
+        Sentence operator()(ConnectedSentence s) const { return s; }
+        Sentence operator()(NotSentence s) const { return s; }
+
         Sentence operator()(ImplySentence s) const { return s; }
         Sentence operator()(ExistsSentence s) const { return s; }
         Sentence operator()(ForallSentence s) const { return s; }
