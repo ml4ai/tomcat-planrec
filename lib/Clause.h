dce29c3be4ea8c2c3ae70a3b3ad531f068d5f6ac
Free-Quarks <lieffers@email.arizona.edu>
2021-07-16 19:11:44 -0700

    rough first go on implementing ask and ask_vars

--- lib/Clause.h
+++ lib/Clause.h
@@ -1,9 +1,89 @@
 #pragma once
 
 #include <vector>
-
+#include "unification.h"
 #include "parsing/ast.hpp"
 
 struct Clause {
     std::vector<ast::Literal<ast::Term>> literals;
+
+    // this will run a binary inference between the 2 clauses and return the resulting clause (an empty clause means these clauses are in contradiction)
+    friend Clause operator==(const Clause& lhs, const Clause& rhs) {
+        typedef std::vector<ast::Literal<ast::Term>> lits;
+        Clause resolvant;
+        lits pos_lits_rhs;
+        lits neg_lits_rhs;
+        lits pos_lits_lhs;
+        lits neg_lits_lhs;
+
+        // create the list of positive and negative literals for each clause
+        for (ast::Literal l1 : rhs.literals ) {
+            if(l1.is_negative) {
+                neg_lits_rhs.push_back(l1);
+            }
+            else {
+                pos_lits_rhs.push_back(l1);
+            }
+        }
+        for (ast::Literal l2 : lhs.literals) {
+            if(l2.is_negative) {
+                neg_lits_lhs.push_back(l2);
+            }
+            else {
+                pos_lits_lhs.push_back(l2);
+            }
+        }
+
+        lits t_pos_lits;
+        lits t_neg_lits;
+        lits resolvant_lits;
+
+        // this loop will first try and resolve the rhs_pos with the lhs_neg and then the rhs_neg with the lhs_pos
+        for (int i=0, i<2, i++) {
+            t_pos_lits.clear();
+            t_neg_lits.clear();
+
+            if (i==0) {
+                t_pos_lits.insert(t_pos_lits.end(), pos_lits_rhs.begin(), pos_lits_rhs.end());
+                t_neg_lits.insert(t_neg_lits.end(), neg_lits_lhs.begin(), neg_lits_lhs.end());
+            }
+            else {
+                t_pos_lits.insert(t_pos_lits.end(), pos_lits_lhs.begin(), pos_lits_lhs.end());
+                t_neg_lits.insert(t_neg_lits.end(), neg_lits_rhs.begin(), neg_lits_rhs.end());                
+            }
+            for(ast::Literal litp : t_pos_lits) {
+                bool found=false;
+                for(ast::Literal litn : t_neg_lits) {
+                    if(litp == litn) {
+                        found = true;
+                    }
+                }
+                if(found == false) {
+                    resolvant_lits.push_back(litp) // if there was no matching negative literal to cancel the positive add it to list
+                }
+            }
+            for(ast::Literal litn : t_neg_lits) {
+                bool found=false;
+                for(ast::Literal litp : t_pos_lits) {
+                    if(litp == litn) {
+                        found = true;
+                    }
+                }
+                if(found == false) {
+                    resolvant_lits.push_back(litn) // if there was no matching positive literal to cancel the negative add it to list
+                }
+            }
+        }
+        
+        // now we remove any duplicates by converting to a set and then back (assuming the resolvant isn't empty) and create our resolvant clause
+        if(resolvant_lits.size()>0) {
+            set<ast::Literal> s(resolvant_lits.begin(), resolvant_lits.end());
+            resolvant_lits.assign(s.begin(), s.end());
+
+            // now to construct the resolvant clause
+            resolvant.literals.insert(resolvant.literals.end(), resolvant_lits.begin(), resolvant_lits.end())
+        }
+
+        return resolvant;
+    }
 };
--- lib/kb.h
+++ lib/kb.h
@@ -28,7 +28,7 @@ struct KnowledgeBase {
 };
 
 // should this be a method in the KB or of the clause struct?
-// this methos returns whether clause is definite or not, only exactly one positive literal.
+// this method returns whether clause is definite or not, namely, if there is only exactly one positive literal.
 bool isDefiniteClause(Clause c) {
     Literal<Term> lit;
     int i=0;
@@ -56,16 +56,71 @@ void tell(KnowledgeBase& kb, ast::Sentence sentence) {
                   kb.facts.push_back(literal);
               },
               [&](Clause clause) {
-                  if (isDefiniteClause(clause)) {
-                      kb.definite_clauses.push_back(clause);
-                  }
-                  else {
-                      kb.clauses.push_back(clause);
-                  }
+                    kb.clauses.push_back(clause); // no definites for resolution inference
                   //for (auto literal : clause.literals) {
                       //kb.clauses.push_back(Clause{{literal}});
                   //}
               },
           },
           CNF_s);
+}
+
+// This will be the ask function for non-variable resolution
+bool ask(KnowledgeBase& kb, ast::Sentence query) {
+    // first we need to not the input sentence
+    ast::Sentence not_query;
+    not_query = query; // here we would apply some method to not the input parsed sentence
+
+    // convert the input query into CNF form
+    ast::Sentence cnf_query;
+    cnf_query = ast::to_CNF(not_query);
+    ast::CNF query_clauses(cnf_query);
+
+    // now to start the resolution inference algorithm, this is just checking clauses
+    // only need one clause to return empty because then the whole sentence is false, if want to check each clause, ask for each clause
+    for (Clause c_kb : kb.clauses) {
+        for (Clause c_q : query_clauses) {
+            if((c_kb==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
+                return true;
+            }
+        }
+    }
+
+    // now we check against the facts of kb incase the clauses didn't resolve it
+    Clause kb_facts;
+    kb_facts.literals.insert(literals.end(), kb.facts.begin(), kb.facts.end());
+
+    for (Clause c_kb : kb_facts) {
+        for (Clause c_q : query_clauses) {
+            if((c_kb==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
+                return true;
+            }
+        }
+    }
+    return false;
+
+}
+
+
+// now for the ask_vars method, which will return a substitution list for a variable in the query, if resolute 
+// we do not have the standard aparting implemented right now, we are operating on each variable input will have a 
+// unqiue name
+
+// unless we restrict ourselves to horn clauses the inputs to the ask_vars will have to be a literal and it will just be unified against the facts
+// of the kb. AIMA p.301 for detials.
+sub_list ask_vars(KnowledgeBase& kb, ast::Literal query) {
+    
+    sub_list sub;
+    for(Literal lit : kb.facts) {
+        sub_list* temp = new sub_list;
+        temp = unify(lit, query, temp);
+        if(!holds_alternative<string>(temp)){
+
+            get<sub_list_type>(sub).insert(get<sub_list_type>(temp).begin(), get<sub_list_type>(temp).end());
+            get<sub_list_type>(temp).clear();
+        }
+        delete temp;
+    }
+    return sub;
+
 }
