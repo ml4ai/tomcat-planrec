28a65e59d384cc09c04af0e078b133416ce8b89f
Free-Quarks <lieffers@email.arizona.edu>
2021-07-20 12:59:23 -0700

    some updates on the ask and tell functions

--- lib/Clause.h
+++ lib/Clause.h
@@ -17,7 +17,7 @@ struct Clause {
         lits neg_lits_lhs;
 
         // create the list of positive and negative literals for each clause
-        for (ast::Literal l1 : rhs.literals ) {
+        for (ast::Literal<ast::Term> l1 : rhs.literals ) {
             if(l1.is_negative) {
                 neg_lits_rhs.push_back(l1);
             }
@@ -25,7 +25,7 @@ struct Clause {
                 pos_lits_rhs.push_back(l1);
             }
         }
-        for (ast::Literal l2 : lhs.literals) {
+        for (ast::Literal<ast::Term> l2 : lhs.literals) {
             if(l2.is_negative) {
                 neg_lits_lhs.push_back(l2);
             }
@@ -39,7 +39,7 @@ struct Clause {
         lits resolvant_lits;
 
         // this loop will first try and resolve the rhs_pos with the lhs_neg and then the rhs_neg with the lhs_pos
-        for (int i=0, i<2, i++) {
+        for (int i=0; i<2; i++) {
             t_pos_lits.clear();
             t_neg_lits.clear();
 
@@ -51,7 +51,7 @@ struct Clause {
                 t_pos_lits.insert(t_pos_lits.end(), pos_lits_lhs.begin(), pos_lits_lhs.end());
                 t_neg_lits.insert(t_neg_lits.end(), neg_lits_rhs.begin(), neg_lits_rhs.end());                
             }
-            for(ast::Literal litp : t_pos_lits) {
+            for(ast::Literal<ast::Term> litp : t_pos_lits) {
                 bool found=false;
                 for(ast::Literal litn : t_neg_lits) {
                     if(litp == litn) {
@@ -59,10 +59,10 @@ struct Clause {
                     }
                 }
                 if(found == false) {
-                    resolvant_lits.push_back(litp) // if there was no matching negative literal to cancel the positive add it to list
+                    resolvant_lits.push_back(litp); // if there was no matching negative literal to cancel the positive add it to list
                 }
             }
-            for(ast::Literal litn : t_neg_lits) {
+            for(ast::Literal<ast::Term> litn : t_neg_lits) {
                 bool found=false;
                 for(ast::Literal litp : t_pos_lits) {
                     if(litp == litn) {
@@ -70,18 +70,18 @@ struct Clause {
                     }
                 }
                 if(found == false) {
-                    resolvant_lits.push_back(litn) // if there was no matching positive literal to cancel the negative add it to list
+                    resolvant_lits.push_back(litn); // if there was no matching positive literal to cancel the negative add it to list
                 }
             }
         }
-        
+
         // now we remove any duplicates by converting to a set and then back (assuming the resolvant isn't empty) and create our resolvant clause
         if(resolvant_lits.size()>0) {
-            set<ast::Literal> s(resolvant_lits.begin(), resolvant_lits.end());
+            set<ast::Literal<Term>> s(resolvant_lits.begin(), resolvant_lits.end());
             resolvant_lits.assign(s.begin(), s.end());
 
             // now to construct the resolvant clause
-            resolvant.literals.insert(resolvant.literals.end(), resolvant_lits.begin(), resolvant_lits.end())
+            resolvant.literals.insert(resolvant.literals.end(), resolvant_lits.begin(), resolvant_lits.end());
         }
 
         return resolvant;
--- lib/kb.h
+++ lib/kb.h
@@ -66,11 +66,19 @@ void tell(KnowledgeBase& kb, ast::Sentence sentence) {
 };
 
 // This is part of my permutation algorithm for permuting the literals to get CNF from DNF
+// check this out again
+// c1 should get appended by each literal in c2, making c2.literals.size() number of output clauses
+// example: c1: [a] | c2: [d,e] then output: [[a,d],[a,e]]
 ast::CNF permute_step(Clause c1, Clause c2) {
-    ast::CNF output.conjunctionOfClauses(c2.literals.size()); // intitalize a CNF with a number of clauses equal to the number of literals in c2
-    int i;
-    for(int i=0, i < c2.literals.size(), i++) {
-        output.conjunctionOfClauses.push_back(c1.push_back(i)) // for each literal in c2, append it to c1 and add that new clause to CNF
+    ast::Sentence for_cnf;
+    ast::CNF output = ast::construct(for_cnf);
+    Clause temp;
+    temp.literals.insert(temp.literals.begin(), c1.literals.begin(), c1.literals.end());
+    for(Literal<Term> l : c2.literals) {
+        temp.literals.insert(temp.literals.begin(), c1.literals.begin(), c1.literals.end()); // sets up original input
+        temp.literals.push_back(l); // append literal in c2 to c1 
+        output.conjunctionOfClauses.push_back(temp); // append that new clause to CNF
+        temp.literals.clear(); // reset the clause
     }
     return output;
 }
@@ -79,7 +87,7 @@ ast::CNF permute_step(Clause c1, Clause c2) {
 // this method not's a CNF sentence
 ast::CNF not_CNF(ast::CNF cnf) {
     // after not'ing the cnf we get a disjunction of conjuctions since all the or's go to and's and vice versa. All the literals are not'd too
-    for (Clause c : cnf) {
+    for (Clause c : cnf.conjunctionOfClauses) {
         for (Literal<Term> lit : c.literals) {
             if (lit.is_negative == false) {
                 lit.is_negative = true;
@@ -94,16 +102,19 @@ ast::CNF not_CNF(ast::CNF cnf) {
     // the application of the distribution rule over the disjunctions and conjuctions now results in taking one literal from each "conjuctive-clause"
     // in the dnf sentence and making a clause out of it, and then permuting through all options conjucting each clause, which is a cnf sentence at the end
     Clause start;
-    ast::CNF temp1, temp2, output;
+    ast::Sentence for_cnf;
+    ast::CNF temp1 = ast::construct(for_cnf);
+    ast::CNF temp2 = ast::construct(for_cnf);
+    ast::CNF output = ast::construct(for_cnf);
     output.conjunctionOfClauses.push_back(start);
-    for (Clause c : CNF) {
+    for (Clause c : cnf.conjunctionOfClauses) {
         for(Clause c1 : output.conjunctionOfClauses) {
             temp1 = permute_step(c1,c);
             temp2.conjunctionOfClauses.insert(temp2.conjunctionOfClauses.end(), temp1.conjunctionOfClauses.begin(), temp1.conjunctionOfClauses.end());
-            temp1.conjunctionOfClauses.clear()
+            temp1.conjunctionOfClauses.clear();
         }
         output.conjunctionOfClauses.clear();
-        output.insert(output.conjunctionOfClauses.end(), temp2.conjunctionOfClauses.begin(), temp2.conjunctionOfClauses.end());
+        output.conjunctionOfClauses.insert(output.conjunctionOfClauses.end(), temp2.conjunctionOfClauses.begin(), temp2.conjunctionOfClauses.end());
         temp2.conjunctionOfClauses.clear();
     }
     return output;
@@ -114,8 +125,9 @@ ast::CNF not_CNF(ast::CNF cnf) {
 bool ask(KnowledgeBase& kb, ast::Sentence query) {
     // convert the input query into CNF form
     ast::CNF cnf_query = ast::construct(query);
-    // now we not the input, note this causes an expoential increase in the sentence size
-    ast::CNF query_clauses;
+    // now we not the input, note this causes an expoential increase in the sentence size, do I need a sentence to make CNF's?
+    ast::Sentence for_cnf;
+    ast::CNF query_clauses = ast::construct(for_cnf);
     query_clauses = not_CNF(cnf_query);
     // now to start the resolution inference algorithm, this is just checking clauses
     // only need one clause to return empty because then the whole sentence is false, if want to check each clause, ask for each clause
@@ -128,12 +140,10 @@ bool ask(KnowledgeBase& kb, ast::Sentence query) {
     }
     // now we check against the facts of kb incase the clauses didn't resolve it
     Clause kb_facts;
-    kb_facts.literals.insert(literals.end(), kb.facts.begin(), kb.facts.end());
-    for (Clause c_kb : kb_facts) {
-        for (Clause c_q : query_clauses.conjunctionOfClauses) {
-            if((c_kb==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
-                return true;
-            }
+    kb_facts.literals.insert(kb_facts.literals.end(), kb.facts.begin(), kb.facts.end());
+    for (Clause c_q : query_clauses.conjunctionOfClauses) {
+        if((kb_facts==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
+            return true;
         }
     }
     return false;
@@ -141,6 +151,8 @@ bool ask(KnowledgeBase& kb, ast::Sentence query) {
  // overloaded option for CNF input instead of parsed sentence
 bool ask(KnowledgeBase& kb, ast::CNF query) {
     // convert the input query into CNF form
+    ast::Sentence for_cnf;
+    ast::CNF query_clauses = ast::construct(for_cnf);
     query_clauses = not_CNF(query);
     // now to start the resolution inference algorithm, this is just checking clauses
     // only need one clause to return empty because then the whole sentence is false, if want to check each clause, ask for each clause
@@ -153,12 +165,10 @@ bool ask(KnowledgeBase& kb, ast::CNF query) {
     }
     // now we check against the facts of kb incase the clauses didn't resolve it
     Clause kb_facts;
-    kb_facts.literals.insert(literals.end(), kb.facts.begin(), kb.facts.end());
-    for (Clause c_kb : kb_facts) {
-        for (Clause c_q : query_clauses.conjunctionOfClauses) {
-            if((c_kb==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
-                return true;
-            }
+    kb_facts.literals.insert(kb_facts.literals.end(), kb.facts.begin(), kb.facts.end());
+    for (Clause c_q : query_clauses.conjunctionOfClauses) {
+        if((kb_facts==c_q).literals.size() == 0) { // if we ever get a resolvant that is in contradiction with our kb, the resolution is true
+            return true;
         }
     }
     return false;
@@ -170,19 +180,16 @@ bool ask(KnowledgeBase& kb, ast::CNF query) {
 
 // unless we restrict ourselves to horn clauses the inputs to the ask_vars will have to be a literal and it will just be unified against the facts
 // of the kb. AIMA p.301 for detials.
-sub_list ask_vars(KnowledgeBase& kb, ast::Literal query) {
-    
+sub_list ask_vars(KnowledgeBase& kb, ast::Literal<Term> query) {    
     sub_list sub;
     for(Literal lit : kb.facts) {
         sub_list temp;
         temp = unify(lit, query, temp);
         if(!holds_alternative<string>(temp)){
-
             get<sub_list_type>(sub).insert(get<sub_list_type>(temp).begin(), get<sub_list_type>(temp).end());
             get<sub_list_type>(temp).clear();
         }
-        delete temp;
+        delete temp; // This isn't working
     }
     return sub;
-
 }
