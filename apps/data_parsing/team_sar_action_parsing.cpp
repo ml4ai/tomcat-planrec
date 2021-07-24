5533aef45da4b10aa4e791056e1719dfad0dbbe7
Loren Champlin <lorenchamplin@SBS-7185.local>
2021-07-24 12:32:22 -0700

    Renamed and moved some code into header file

---
+++ apps/data_parsing/team_sar_action_parsing.cpp
@@ -0,0 +1,10 @@
+#include "parsers/team_sar_parser.h"
+
+
+
+int main() {
+
+  std::string infile = "../apps/data_parsing/study-2_2021.06_HSRData_TrialMessages_Trial-T000485_Team-TM000143_Member-na_CondBtwn-2_CondWin-SaturnA_Vers-2.metadata";
+  team_sar_parser(infile,true,"team_sar_ppt.json");
+  return EXIT_SUCCESS;
+}
