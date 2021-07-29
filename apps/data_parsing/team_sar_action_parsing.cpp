87d270b220de4e5aa83bcf297080fa6b231b4aa4
Loren Champlin <lorenchamplin@SBS-7185.local>
2021-07-29 11:12:09 -0700

    Saving progress

--- apps/data_parsing/team_sar_action_parsing.cpp
+++ apps/data_parsing/team_sar_action_parsing.cpp
@@ -3,7 +3,6 @@
 
 
 int main() {
-
   std::string infile = "../apps/data_parsing/study-2_2021.06_HSRData_TrialMessages_Trial-T000485_Team-TM000143_Member-na_CondBtwn-2_CondWin-SaturnA_Vers-2.metadata";
   team_sar_parser(infile,true,"team_sar_ppt.json");
   return EXIT_SUCCESS;
