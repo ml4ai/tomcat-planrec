223e3c586e6f694acf6ce99e2a7d2c77a8bab32e
Loren Champlin <lorenchamplin@SBS-7185.local>
2021-10-22 13:00:14 -0700

    Doesn't need to track tree

--- lib/cpphop/cppDFSplanrec.h
+++ lib/cpphop/cppDFSplanrec.h
@@ -25,70 +25,58 @@ struct tNode {
     CFM cfm;
     json team_plan;
     double likelihood;
-    std::vector<int> successors = {};
 };
-
-template <class State>
-using tTree = std::vector<tNode<State>>;
-
-template <class State>
-int add_node(tNode<State>& n,tTree<State>& t) {
-  t.push_back(n);
-  return t.size() - 1;
-}
 
 template <class State, class Domain>
 CFM planrecDFS(json& data_team_plan,
-                  tTree<State>& t,
+                  tNode<State> v,
                   CPM& cpm,
-                  int v,
                   Domain& domain,
                   int seed) {
   CFM max_cfm;
   std::mt19937 gen(seed);
   std::uniform_real_distribution<double> dis(0.0,1.0);
   double max_likelihood = log(0.0); 
-  std::stack<int> S;
+  std::stack<tNode<State>> S;
   S.push(v);
   while (!S.empty()) {
     v = S.top();
     S.pop();
-    if (t[v].team_plan["size"] >= data_team_plan["size"] ||
-        t[v].tasks.empty()) {
-      if (t[v].team_plan == data_team_plan) {
-        if (t[v].likelihood >= max_likelihood) {
-          max_cfm = t[v].cfm;
-          max_likelihood = t[v].likelihood;
+    if (v.team_plan["size"] >= data_team_plan["size"] ||
+        v.tasks.empty()) {
+      if (v.team_plan == data_team_plan) {
+        if (v.likelihood >= max_likelihood) {
+          max_cfm = v.cfm;
+          max_likelihood = v.likelihood;
         }
       }
       continue;
     }
 
-    Task task = t[v].tasks.back();
+    Task task = v.tasks.back();
 
     if (in(task.task_id, domain.operators)) {
       Operator<State> op = domain.operators[task.task_id];
-      std::optional<State> newstate = op(t[v].state, task.args);
+      std::optional<State> newstate = op(v.state, task.args);
       if (newstate) {
         pOperator<State> pop = domain.poperators[task.task_id];
         tNode<State> n;
         n.state = newstate.value();
-        n.likelihood = t[v].likelihood + log(pop(t[v].state,n.state,task.args));
+        n.likelihood = v.likelihood + log(pop(v.state,n.state,task.args));
         if (n.likelihood < max_likelihood) {
           continue;
         }
-        n.tasks = t[v].tasks;
+        n.tasks = v.tasks;
         n.tasks.pop_back();
-        n.team_plan = t[v].team_plan;
+        n.team_plan = v.team_plan;
         json g;
         g["task"] = task2string(task);
         for (auto a : task.agents) {
           n.team_plan["plan"][a].push_back(g);
           n.team_plan["size"] = 1 + n.team_plan["size"].template get<int>();
         }
-        int w = add_node(n, t);
-        t[v].successors.push_back(w);
-        S.push(w);
+        n.cfm = v.cfm;
+        S.push(n);
       }
       continue;
     }
@@ -98,7 +86,7 @@ CFM planrecDFS(json& data_team_plan,
       std::vector<cTasks> c = {};
       std::string key = ""; 
       for (auto cmethod : relevant) {
-        cTasks subtasks = cmethod(t[v].state,task.args); 
+        cTasks subtasks = cmethod(v.state,task.args); 
         if (subtasks.first != "NIL") {
           c.push_back(subtasks);
           if (subtasks.first != "U") {
@@ -115,38 +103,38 @@ CFM planrecDFS(json& data_team_plan,
           if (cpm.find(key) != cpm.end()) {
             if (cpm[key].find(task.task_id) != cpm[key].end()) {
               if (cpm[key][task.task_id].find(m.first) != cpm[key][task.task_id].end()){
-                n.likelihood = t[v].likelihood + log(cpm[key][task.task_id][m.first]);
+                n.likelihood = v.likelihood + log(cpm[key][task.task_id][m.first]);
               }
               else {
                 cpm[key][task.task_id][m.first] = dis(gen); 
-                n.likelihood = t[v].likelihood + log(cpm[key][task.task_id][m.first]);
+                n.likelihood = v.likelihood + log(cpm[key][task.task_id][m.first]);
               }
             } 
             else {
               cpm[key][task.task_id][m.first] = dis(gen);
-              n.likelihood = t[v].likelihood + log(cpm[key][task.task_id][m.first]);
+              n.likelihood = v.likelihood + log(cpm[key][task.task_id][m.first]);
             }
           }
           else {
             cpm[key][task.task_id][m.first] = dis(gen);
-            n.likelihood = t[v].likelihood + log(cpm[key][task.task_id][m.first]);
+            n.likelihood = v.likelihood + log(cpm[key][task.task_id][m.first]);
           }
         }
         else {
-          n.likelihood = t[v].likelihood + log(1.0/c.size());
+          n.likelihood = v.likelihood + log(1.0/c.size());
         }
         if (n.likelihood < max_likelihood) {
           continue;
         }
-        n.state = t[v].state;
-        n.tasks = t[v].tasks;
+        n.state = v.state;
+        n.tasks = v.tasks;
         n.tasks.pop_back();
-        n.team_plan = t[v].team_plan;
+        n.team_plan = v.team_plan;
         for (auto i = m.second.end();
             i != m.second.begin();) {
           n.tasks.push_back(*(--i));
         }
-        n.cfm = t[v].cfm;
+        n.cfm = v.cfm;
         if (m.first != "U") {
           if (n.cfm.find(key) != n.cfm.end()) {
             if (n.cfm[key].find(task.task_id) != n.cfm[key].end()) {
@@ -165,9 +153,7 @@ CFM planrecDFS(json& data_team_plan,
             n.cfm[key][task.task_id][m.first] = 1;
           }
         }
-        int w = add_node(n,t);
-        t[v].successors.push_back(w);
-        S.push(w);
+        S.push(n);
       }
       continue;
     }
@@ -186,15 +172,13 @@ CFM cppDFSplanrec(json& data_team_plan,
                              Tasks tasks, 
                              Domain& domain,
                              int seed) {
-  tTree<State> t;
   tNode<State> root;
   root.state = state;
   root.tasks = tasks;
   root.team_plan["size"] = 0;
   root.cfm = {};
   root.likelihood = 0.0;
-  int w = add_node(root,t);
-  return planrecDFS(data_team_plan,t,cpm,w,domain,seed);
+  return planrecDFS(data_team_plan,root,cpm,domain,seed);
 }
 
 CFM sumCFMs(std::vector<CFM>& cfms) {
