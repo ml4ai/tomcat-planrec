f3d10d2207341cbafbd4d156a7a1c7ca2a8c40d9
Adarsh Pyarelal <adarsh@arizona.edu>
2022-02-21 16:05:32 -0700

    adding namespace

--- lib/fol/util.h
+++ lib/fol/util.h
@@ -3,6 +3,6 @@
 #include "Term.h"
 
 // Get name of term
-std::string name(Term term) {
+std::string name(ast::Term term) {
     return boost::apply_visitor([](const auto& t) { return t.name; }, term);
 }
