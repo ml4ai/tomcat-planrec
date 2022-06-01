28bfd8d1e76254df7ba5e822d0c1c37b58616b15
ualiangzhang <liangzh@arizona.edu>
2022-06-01 16:41:22 -0700

    add one more example to test z3

--- test/test_z3.cpp
+++ test/test_z3.cpp
@@ -167,28 +167,64 @@ string test_logic_infer6() {
     std::cout << "test logic\n";
     z3::context c;
     z3::solver s(c);
-    s.from_string("(declare-fun x () Bool)\n"
-                  " (declare-fun y () Bool)\n"
-                  " (declare-fun John () Bool)\n"
-                  " (declare-fun Anil () Bool)\n"
-                  " (declare-fun Harry () Bool)\n"
-                  " (declare-fun Apple () Bool)\n"
-                  " (declare-fun Peanuts () Bool)\n"
-                  " (declare-fun vegetables () Bool)\n"
-                  " (declare-fun food (Bool) Bool)\n"
-                  " (declare-fun likes (Bool Bool) Bool)\n"
-                  " (declare-fun eats (Bool Bool) Bool)\n"
-                  " (declare-fun killed (Bool) Bool)\n"
-                  " (declare-fun alive (Bool) Bool)\n"
-                  " (assert (forall ((x Bool)) (=> (food x) (likes John x))))\n"
-                  " (assert (and (food Apple) (food vegetables)))\n"
-                  " (assert (forall ((x Bool)) (forall ((y Bool)) (=> (and (eats x y) (not (killed x))) (food y)))))\n"
-                  " (assert (and (eats Anil Peanuts) (alive Anil)))\n"
-                  " (assert (forall ((x Bool)) (=> (eats Anil x) (eats Harry x))))\n"
-                  " (assert (forall ((x Bool)) (=> (not (killed x)) (alive x))))\n"
-                  " (assert (forall ((x Bool)) (=> (alive x) (not (killed x)))))\n"
-                  " (assert (not (likes John Peanuts)))"
-                  );
+    s.from_string(
+        "(declare-fun x () Bool)\n"
+        " (declare-fun y () Bool)\n"
+        " (declare-fun John () Bool)\n"
+        " (declare-fun Anil () Bool)\n"
+        " (declare-fun Harry () Bool)\n"
+        " (declare-fun Apple () Bool)\n"
+        " (declare-fun Peanuts () Bool)\n"
+        " (declare-fun vegetables () Bool)\n"
+        " (declare-fun food (Bool) Bool)\n"
+        " (declare-fun likes (Bool Bool) Bool)\n"
+        " (declare-fun eats (Bool Bool) Bool)\n"
+        " (declare-fun killed (Bool) Bool)\n"
+        " (declare-fun alive (Bool) Bool)\n"
+        " (assert (forall ((x Bool)) (=> (food x) (likes John x))))\n"
+        " (assert (and (food Apple) (food vegetables)))\n"
+        " (assert (forall ((x Bool)) (forall ((y Bool)) (=> (and (eats x y) "
+        "(not (killed x))) (food y)))))\n"
+        " (assert (and (eats Anil Peanuts) (alive Anil)))\n"
+        " (assert (forall ((x Bool)) (=> (eats Anil x) (eats Harry x))))\n"
+        " (assert (forall ((x Bool)) (=> (not (killed x)) (alive x))))\n"
+        " (assert (forall ((x Bool)) (=> (alive x) (not (killed x)))))\n"
+        " (assert (not (likes John Peanuts)))");
+
+    switch (s.check()) {
+    case unsat:
+        return "sat";
+    case sat:
+        return "unsat";
+    case unknown:
+        return "unknown";
+    }
+}
+
+string test_logic_infer7() {
+    std::cout << "test logic\n";
+    z3::context c;
+    z3::solver s(c);
+    s.from_string(
+        "(declare-fun x () Bool)\n"
+        " (declare-fun y () Bool)\n"
+        " (declare-fun z () Bool)\n"
+        " (declare-fun Jack () Bool)\n"
+        " (declare-fun Tuna () Bool)\n"
+        " (declare-fun Curiosity () Bool)\n"
+        " (declare-fun Animal (Bool) Bool)\n"
+        " (declare-fun Loves (Bool Bool) Bool)\n"
+        " (declare-fun Kills (Bool Bool) Bool)\n"
+        " (declare-fun Cat (Bool) Bool)\n"
+        " (assert (forall ((x Bool)) (=> (forall ((y Bool)) (=> (Animal y)\n"
+        " (Loves x y))) (exists ((y Bool)) (Loves y x)))))\n"
+        " (assert (forall ((x Bool)) (=> (exists ((z Bool)) (and (Animal z)\n"
+        " (Kills x z))) (forall ((y Bool)) (not (Loves y x))))))\n"
+        " (assert (forall ((x Bool)) (=> (Animal x) (Loves Jack x))))\n"
+        " (assert (or (Kills Jack Tuna) (Kills Curiosity Tuna)))\n"
+        " (assert (Cat Tuna))\n"
+        " (assert (forall ((x Bool)) (=> (Cat x) (Animal x))))\n"
+        " (assert (not (Kills Curiosity Tuna)))");
 
     switch (s.check()) {
     case unsat:
@@ -208,4 +244,5 @@ BOOST_AUTO_TEST_CASE(test_z3) {
     BOOST_TEST(test_logic_infer4() == "unsat");
     BOOST_TEST(test_logic_infer5() == "sat");
     BOOST_TEST(test_logic_infer6() == "sat");
+    BOOST_TEST(test_logic_infer7() == "sat");
 }
