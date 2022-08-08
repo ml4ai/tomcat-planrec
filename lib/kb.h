f9353aa7b9ca71e67673fd9658164e0fd3f18469
Loren Champlin <champlin@email.arizona.edu>
2022-08-07 21:13:16 -0700

    Adding type inheritence

--- lib/kb.h
+++ lib/kb.h
@@ -11,17 +11,69 @@
 
 using namespace fol;
 
+struct Type {
+  std::string type;
+  std::vector<int> lineage;
+  std::vector<int> descendents;
+};
+
+struct TypeTree {
+  std::unordered_map<int,Type> types;
+  int nextID = 0;
+  std::vector<int> freedIDs;
+  int add_type(Type& type) {
+    int id;
+    if (!freedIDs.empty()) {
+      id = freedIDs.back();
+      freedIDs.pop_back();
+    }
+    else {
+      id = nextID;
+      nextID++;
+    }
+  }
+  types[id] = type;
+  return id;
+
+  int find_type(std::string type) {
+    for (auto const &[i,t] : this->types) {
+      if (t.type == type) {
+        return i;
+      }  
+    }
+    std::string msg = "Type ";
+    msg += type;
+    msg += " not found!";
+    throw std::logic_error(msg);
+  }
+};
+
 struct KnowledgeBase {
-    // data_type: {candidate1, candidate2, ...}
-    std::map<std::string, std::vector<std::string>> data_types;
-    // symbol: data_type
-    std::map<std::string, std::string> symbols;
+    TypeTree typetree;
+    int root; 
     // predicate: predicate data_type1 data_type2
-    std::map<std::string, std::vector<std::string>> predicates;
+    std::unordered_map<std::string, std::unordered_map<std::string,std::string>> predicates;
     // The true facts updating from the message bus
-    std::map<std::string, std::set<std::vector<std::string>>> facts;
-    // The operations defined by the HDDL domain representation
-    std::set<std::string> domain_context;
+    std::set<std::string> pfacts;
+    std::set<std::string> nfacts;
+
+    KnowledgeBase() {
+      Type type;
+      type.type = "Object";
+      this->root = this->typetree.add_type(type);
+    }
+
+    void add_type(std::string type, std::string ancestor) {
+      Type new_t;
+      new_t.type = type;
+      int a = this->typetree.find_type(ancestor); 
+      new_t.lineage.push_back(a);
+      for (auto i : this->typetree.types[a].lineage) {
+        new_t.lineage.push_back(i);
+      }
+      int t = this->typetree.add_type(new_t);
+      this->typetree.types[a].descendents.push_back(t);
+    }
 };
 
 void initialize_data_type(KnowledgeBase& kb,
