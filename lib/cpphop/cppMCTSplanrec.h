caecaaccf2c34cb7b1c46105b493eb894d0dee52
Loren Champlin <champlin@email.arizona.edu>
2022-09-22 09:18:50 -0700

    Updating using HDDL mechanics

--- lib/cpphop/cppMCTSplanrec.h
+++ lib/cpphop/cppMCTSplanrec.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "../util.h"
+#include "../typedefs.h"
+//#include "printing.h"
 #include <any>
 #include <iostream>
 #include <optional>
@@ -8,230 +11,90 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include "../util.h"
-#include <nlohmann/json.hpp>
-#include <math.h>
-#include <random>
-#include <cfloat>
+#include <limits>
+#include "cppMCTShop.h"
 
-using json = nlohmann::json;
-
-template <class State>
-struct prNode {
-    State state;
-    Tasks tasks;
-    json team_plan;
-    double mean = 0.0;
-    int sims = 0;
-    int pred = -1;
-    std::vector<int> successors = {};
-};
-
-template <class State>
-struct prTree {
-  std::unordered_map<int,prNode<State>> nodes;
-  int nextID = 0;
-  std::vector<int> freedIDs;
-  int add_node(prNode<State>& n) {
-    int id;
-    if (!freedIDs.empty()) {
-      id = freedIDs.back();
-      freedIDs.pop_back();
-    }
-    else {
-      id = nextID;
-      nextID++;
-    }
-    nodes[id] = n;
-    return id;
-  }
-  void delete_node(int id) {
-    if (nodes.erase(id)) {
-      freedIDs.push_back(id);
-    }
-    return;
-  }
-};
-
-template <class State>
-struct prResults {
-  prTree<State> t;
-  int root;
-  int end;
-};
-
-bool subseq(json& j, json& g) {
-  for (int t = 0; t < j.size(); t++) {
-    if (j[t] != g[t]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool subsequence(json& j, json& g, std::vector<std::string>& agents) {
-
-  for (auto const &a : agents) {
-    if (j["plan"][a].size() <= g["plan"][a].size()) {
-      if (!subseq(j["plan"][a], g["plan"][a])) {
+bool is_subseq(std::vector<std::string> v1, std::vector<std::string> v2) {
+  if (v1.size() <= v2.size()) {
+    for (int i = 0; i < v1.size(); i++) {
+      if (v1[i] != v2[i]) {
         return false;
       }
     }
-    else {
+    return true;
+  }
+  for (int i = 0; i < v2.size(); i++) {
+    if (v1[i] != v2[i]) {
       return false;
     }
   }
   return true;
 }
 
-template <class State>
-int
-cselection_rec(prTree<State>& t,
-          int v,
-          double eps,
-          int seed = 2021) {
-
-    std::mt19937 gen(seed);
-    std::uniform_real_distribution<double> dis(0.0,1.0);
-    while (!t.nodes[v].successors.empty()) {
-      double e = dis(gen);
-      if (e > eps) {
-        std::vector<double> r_maxes = {};
-        r_maxes.push_back(t.nodes[v].successors.front());
-        double r_max = t.nodes[r_maxes.back()].mean;
-        for (auto const &w : t.nodes[v].successors) {
-            if (t.nodes[w].sims == 0) {
-              return w;
-            }
-            double s = t.nodes[w].mean;
-            if (s >= r_max) {
-              if (s > r_max) {
-                r_max = s;
-                r_maxes.clear();
-                r_maxes.push_back(w);
-              }
-              else {
-                r_maxes.push_back(w);
-              }
-            }
-        }
-        std::vector<double> v_maxes = {};
-        v_maxes.push_back(r_maxes.front());
-        int v_max = t.nodes[v_maxes.back()].sims;
-        for (auto const &w : r_maxes) {
-          int s = t.nodes[w].sims;
-          if (s >= v_max) {
-            if (s > v_max) {
-              v_max = s;
-              v_maxes.clear();
-              v_maxes.push_back(w);
-            }
-            else {
-              v_maxes.push_back(w);
-            }
-          }
-        }
-        seed++;
-        v = *select_randomly(v_maxes.begin(), v_maxes.end(), seed);
-        seed++;
-      }
-      else {
-        for (auto const &w : t.nodes[v].successors) {
-          if (t.nodes[w].sims == 0) {
-            return w;
-          }
-        }
-        seed++;
-        v = *select_randomly(t.nodes[v].successors.begin(), t.nodes[v].successors.end(), seed);
-        seed++;
-      }
-    }
-    return v;
-}
-
-template <class State>
-void cbackprop_rec(prTree<State>& t, int n, double r) {
-  do {
-    if (t.nodes[n].successors.empty()) {
-      t.nodes[n].mean = r;
-      t.nodes[n].sims++;
-    }
-    else {
-      t.nodes[n].mean = (r + t.nodes[n].sims*t.nodes[n].mean)/(t.nodes[n].sims + 1);
-      t.nodes[n].sims++;
-    }
-    n = t.nodes[n].pred;
-  }
-  while (n != -1);
-  return;
-}
-
-template <class State, class Domain>
 double
-csimulation_rec(int horizon,
-           json& data_team_plan,
-           json team_plan,
-           State state,
-           Tasks tasks,
-           Domain& domain,
-           int seed) {
-
-    int h = 0;
-    while (team_plan["size"] < data_team_plan["size"] && h < horizon) {
-      Task task = tasks.back();
-
-      if (in(task.task_id, domain.operators)) {
-          Operator<State> op = domain.operators[task.task_id];
-          std::optional<State> newstate = op(state, task.args);
-          if (newstate) {
-              pOperator<State> pop = domain.poperators[task.task_id];
-              tasks.pop_back();
-              json g;
-              g["task"] = task2string(task);
-              state = newstate.value();
-              for (auto const &a : task.agents) {
-                team_plan["plan"][a].push_back(g);
-                team_plan["size"] = 1 + team_plan["size"].get<int>();
-              }
-              seed++;
-              h++;
-              continue;
-          }
-          return 0.0;
-      }
-
-      if (in(task.task_id, domain.cmethods)) {
-        auto relevant = domain.cmethods[task.task_id];
-        std::vector<cTasks> c = {};
-        for (auto const &cmethod : relevant) {
-            cTasks subtasks = cmethod(state, task.args);
-            if (subtasks.first != "NIL") {
-              c.push_back(subtasks);
-            }
-        }
-        seed++;
-        if (c.empty()) {
-          return 0.0;
-        }
-        cTasks r = *select_randomly(c.begin(), c.end(), seed);
-        seed++;
-        tasks.pop_back();
-        for (auto i = r.second.end();
-            i != r.second.begin();) {
-          tasks.push_back(*(--i));
-        }
-        h++;
-        continue;  
-      }
-      std::string message = "Invalid task ";
-      message += task.task_id;
-      message += " during simulation!";
-      throw std::logic_error(message);
-    }
-    if (subsequence(team_plan, data_team_plan, state.agents)) {
+simulation_rec(int horizon,
+               std::vector<std::string> given_plan,
+               std::vector<std::string> plan,
+               KnowledgeBase state,
+               TaskGraph tasks,
+               DomainDef& domain,
+               std::mt19937_64& g,
+               int h = 0) {
+  if (tasks.empty() || h >= horizon) {
+    if (is_subseq(plan,given_plan)) { 
       return domain.score(state);
     }
-    return 0.0;
+    return -1.0; 
+  }
+  h++;
+  for (auto &[i,gt] : tasks.GTs) {
+    if (gt.incoming.empty()) {
+      if (domain.actions.contains(gt.head)) {
+        auto act = domain.actions.at(gt.head).apply(state,gt.args);
+        if (!act.second.empty()) {
+          std::shuffle(act.second.begin(),act.second.end(),g);
+          auto gtasks = tasks;
+          gtasks.remove_node(i);
+          for (auto &ns : act.second) {
+            ns.update_state();
+            double rs = csimulation(horizon,ns,gtasks,domain,g,h);
+            if (rs > -1.0) {
+              return rs;
+            }
+          }
+        }
+        else {
+          return -1.0;
+        }
+      }
+      else if (domain.methods.contains(gt.head)) {
+        auto task_methods = domain.methods[gt.head];
+        std::shuffle(task_methods.begin(),task_methods.end(),g);
+        for (auto &m : task_methods) {
+          auto all_gts = m.apply(state,gt.args,tasks,i);
+          if (!all_gts.second.empty()) {
+            std::shuffle(all_gts.second.begin(),all_gts.second.end(),g);
+            for (auto &gts : all_gts.second) {
+              double rs = csimulation(horizon,state,gts,domain,g,h);
+              if (rs > -1.0) {
+                return rs;
+              }
+            }
+          }
+          else {
+            return -1.0;
+          }
+        }
+      }
+      else {
+        std::string message = "Invalid task ";
+        message += gt.head;
+        message += " during simulation!";
+        throw std::logic_error(message);
+      }
+    }
+  }
+  return -1.0;
 }
 
 template <class State, class Domain>
