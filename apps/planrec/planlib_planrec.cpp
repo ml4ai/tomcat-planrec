34682e808e544fb284231598f80ca331e2a29ea5
Loren Champlin <champlin@email.arizona.edu>
2022-09-30 08:43:37 -0700

    Script for running plan lib planrec algorithm

--- apps/planrec/planlib_planrec.cpp
+++ apps/planrec/planlib_planrec.cpp
@@ -4,6 +4,7 @@
 #include <istream>
 #include <boost/program_options.hpp>
 #include <boost/json.hpp>
+#include "util.h"
 #include "cpphop/loader.h"
 #include "cpphop/planlib_planrec.h"
 #include "cpphop/grapher.h"
@@ -14,9 +15,7 @@ namespace json = boost::json;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  std::string dom_file = "../domains/transport_domain.hddl";
-  std::string prob_file = "../domains/transport_problem.hddl";
-  std::string plan_lib = "../domains/transport_planlib.json";
+  std::string planlib_file = "../domains/transport_planlib.json";
   std::string sample = "delivery_sample";
   int sample_size = 2;
   bool graph = false;
@@ -25,8 +24,6 @@ int main(int argc, char* argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
       ("help,h", "produce help message")
-      ("dom_file,D", po::value<std::string>(),"domain file (string), default = transport_domain.hddl")
-      ("prob_file,P",po::value<std::string>(),"problem file (string), default = transport_problem.hddl")
       ("plan_lib,L",po::value<std::string>(),"Plan Library file, default = transport_planlib.json")
       ("sample,S",po::value<std::string>(),"Plan Rec sample (string), default = delivery_sample")
       ("sample_size,ss",po::value<int>(),"sample size for Plan Rec sample (int), default = 2")
@@ -43,16 +40,8 @@ int main(int argc, char* argv[]) {
       return 0;
     }
 
-    if (vm.count("dom_file")) {
-      dom_file = vm["dom_file"].as<std::string>();
-    }
-
-    if (vm.count("prob_file")) {
-      prob_file = vm["prob_file"].as<std::string>();
-    }
-
     if (vm.count("plan_lib")) {
-      plan_lib = vm["plan_lib"].as<std::string>();
+      planlib_file = vm["plan_lib"].as<std::string>();
     }
 
     if (vm.count("sample")) {
@@ -77,28 +66,29 @@ int main(int argc, char* argv[]) {
   catch(...) {
     std::cerr << "Exception of unknown type!\n";
   }
-  auto [domain,problem] = load(dom_file,prob_file);
+  auto planlib = parse_file(planlib_file.c_str()); 
   auto first = pr_samples[sample].begin();
   auto last = pr_samples[sample].begin() + sample_size;
   std::vector<std::string> given_plan(first,last);
   if (graph) {
     if (graph_file == "") {
-      graph_file = problem.head +".png"; 
+      graph_file = sample +"_inference.png"; 
     }
     auto start = std::chrono::high_resolution_clock::now();
-    auto results = cppMCTSplanrec(domain,problem,given_plan,scorers[score_fun],R,plan_size,eps,successes,prob,seed); 
+    auto results = seek_explanation(planlib.as_object(),given_plan); 
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout << "Time taken by planner: "
+    cout << "Time taken by planrec: "
         << duration.count() << " microseconds" << endl;
-    generate_graph(results.t[results.end].plan,domain,results.tasktree,results.ttRoot,graph_file);
+    std::string root = json::value_to<std::string>(planlib.as_object()["root"]);
+    generate_graph_from_json(results,given_plan.size(),planlib.as_object()["actions"].as_array(),root,graph_file);
   }
   else {
     auto start = std::chrono::high_resolution_clock::now();
-    cppMCTSplanrec(domain,problem,given_plan,scorers[score_fun],R,plan_size,eps,successes,prob,seed); 
+    seek_explanation(planlib.as_object(),given_plan);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout << "Time taken by planner: "
+    cout << "Time taken by planrec: "
         << duration.count() << " microseconds" << endl;
   } 
   return EXIT_SUCCESS;
