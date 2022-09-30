9b07e4974846e2a4d4072d151f9ab446f7296cbd
Loren Champlin <champlin@email.arizona.edu>
2022-09-30 08:45:48 -0700

    fixed some logic errors

--- lib/cpphop/planlib_planrec.h
+++ lib/cpphop/planlib_planrec.h
@@ -8,20 +8,20 @@ namespace json = boost::json;
 json::object seek_explanation(json::object planlib, std::vector<std::string> O) {
   double max_score = -std::numeric_limits<double>::infinity(); 
   json::object arg_max;
-  for (auto const& p : planlib["library"].as_array()) {
-    if (O.size() <= p["plan"].as_array().size()) {
+  for (auto& p : planlib["library"].as_array()) {
+    if (O.size() <= p.as_object()["plan"].as_array().size()) {
       bool match = true;
       for (int i = 0; i < O.size(); i++) {
-        std::string act = json::value_to<std::string>(p["plan"].as_array()[i]);
+        std::string act = json::value_to<std::string>(p.as_object()["plan"].as_array()[i]);
         if (act.find(O[i]) == std::string::npos) {
           match = false;
           break;
         }
       }
       if (match) {
-        if (p["score"].as_double() > max_score) {
-          arg_max = p;
-          max_score = p["score"].as_double();
+        if (p.as_object()["score"].as_double() > max_score) {
+          arg_max = p.as_object();
+          max_score = json::value_to<double>(p.as_object()["score"]);
         }
       }
     }
