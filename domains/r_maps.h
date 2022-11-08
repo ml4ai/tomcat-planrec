48d3a7c8873100e5cfbf7cc2a18f4e21686b5152
Loren Champlin <champlin@email.arizona.edu>
2022-11-08 16:15:12 -0700

    File for domain specific reach maps

---
+++ domains/r_maps.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "typedefs.h"
+
+Reach_Map transport_reach_map = {{"deliver",{"drive","noop","pick_up","drop"}},
+                                 {"get_to",{"drive","noop"}},
+                                 {"load",{"pick_up"}},
+                                 {"unload",{"drop"}},
+                                 {"m_deliver_ordering_0",{"drive","noop","pick_up","drop"}},
+                                 {"m_unload_ordering_0",{"drop"}},
+                                 {"m_load_ordering_0",{"pick_up"}},
+                                 {"m_drive_to_ordering_0",{"drive"}},
+                                 {"m_drive_to_via_ordering_0",{"drive"}},
+                                 {"m_i_am_there_ordering_0", {"noop"}}
+                                };
+
+Reach_Maps reach_maps = {{"transport_reach_map",transport_reach_map}};
