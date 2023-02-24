29586af943ffe5238b82caee275b96484d0a764d
Loren Champlin <champlin@email.arizona.edu>
2023-02-23 21:53:54 -0700

    Adjusted test

--- apps/perc/redis_test.cpp
+++ apps/perc/redis_test.cpp
@@ -6,12 +6,15 @@ using namespace sw::redis;
 int main() {
   try {
     auto redis = Redis("tcp://127.0.0.1:6379");
-    std::vector<std::pair<std::string,std::pair<std::string,std::string>>> xresults;
-    redis.xrange("test_vic","-","+",5,std::back_inserter(xresults));
+    std::vector<std::pair<std::string,std::vector<std::pair<std::string,std::string>>>> xresults;
+    redis.xrange("medic_fov","-","+",50,std::back_inserter(xresults));
     for (auto x : xresults) {
-      std::cout << x.first << " : " << x.second.first << x.second.second << std::endl;
+      std::cout << x.first << " : " << std::endl;
+      for ( auto y : x.second ) {
+        std::cout << y.first << " " << y.second << std::endl;
+      }
     }
-    //redis.flushall();
+    redis.flushall();
   } catch (const Error &e) {
     std::cout << "Failed : " << e.what() << std::endl;
   }
