f59fc93cd8216c7c1a0b76f8ec383fc6e4de8689
Loren Champlin <champlin@email.arizona.edu>
2023-02-25 00:54:55 -0700

    Update test

--- apps/perc/redis_test.cpp
+++ apps/perc/redis_test.cpp
@@ -7,7 +7,7 @@ int main() {
   try {
     auto redis = Redis("tcp://127.0.0.1:6379");
     std::vector<std::pair<std::string,std::vector<std::pair<std::string,std::string>>>> xresults;
-    redis.xrange("medic_fov","-","+",50,std::back_inserter(xresults));
+    redis.xrange("fov","-","+",50,std::back_inserter(xresults));
     for (auto x : xresults) {
       std::cout << x.first << " : " << std::endl;
       for ( auto y : x.second ) {
