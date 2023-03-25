12ad91f2788ac2b17c728edc3cc2168095526853
Loren Champlin <champlin@email.arizona.edu>
2023-03-25 15:25:19 -0700

    Added some parsing flags

--- apps/perc/PercAgent.hpp
+++ apps/perc/PercAgent.hpp
@@ -10,6 +10,12 @@ class PercAgent : public Agent {
     KnowledgeBase kb;
     Redis_Connect* rc;
     Time initial_time;
+    bool start = false;
+    std::string medic_current_loc = "sga";
+    std::string engineer_current_loc = "sga";
+    std::string transporter_current_loc = "sga";
+    std::unordered_set<int> engineer_assist;
+    std::unordered_set<int> transporter_assist;
     void process(mqtt::const_message_ptr msg) override;
 
   public:
