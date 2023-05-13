828ad64af3a07ccc25d24d462cf652d037d17cef
Loren Champlin <champlin@email.arizona.edu>
2023-05-12 22:40:59 -0700

    parses plan explanations on redis database into json files

---
+++ apps/data_parsing/redis_to_json.cpp
@@ -0,0 +1,88 @@
+#include <boost/program_options.hpp>
+#include <boost/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <sw/redis++/redis++.h>
+
+namespace json = boost::json;
+namespace po = boost::program_options;
+
+using namespace sw::redis;
+
+int main(int argc, char* argv[]) {
+  std::string redis_address = "";
+  std::string save_path = "../metadata";
+  std::string outfile = "output";
+  std::string lower = "-";
+  std::string upper = "+";
+
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help,h", "produce help message")
+      ("redis_address,a",po::value<std::string>(),"redis address to redis database (string)")
+      ("save_path,s", po::value<std::string>(), "path to where outputted files are saved (string)")
+      ("outfile,o",po::value<std::string>(),"name for outputs (string)")
+      ("lower_bound,l", po::value<std::string>(),"lower bound for redis retrieval (string)")
+      ("upper_bound,u", po::value<std::string>(),"upper bound for redis retrieval (string)")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+    if (vm.count("redis_address")) {
+      redis_address = vm["redis_address"].as<std::string>();
+    }
+
+    if (vm.count("save_path")) {
+      save_path = vm["save_path"].as<std::string>();
+    }
+
+    if (vm.count("outfile")) {
+      outfile = vm["outfile"].as<std::string>();
+    }
+
+    if (vm.count("lower_bound")) {
+      lower = vm["lower_bound"].as<std::string>();
+    }
+
+    if (vm.count("upper_bound")) {
+      upper = vm["upper_bound"].as<std::string>();
+    }
+
+  }
+  catch(std::exception& e) {
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+  }
+  catch(...) {
+    std::cerr << "Exception of unknown type!\n";
+  }
+  
+  try {
+    auto redis = Redis(redis_address);
+    std::vector<std::pair<std::string,std::vector<std::pair<std::string,std::string>>>> xresults;
+    redis.xrange("explanations",lower,upper,std::back_inserter(xresults));
+    int i = 0;
+    for (auto x : xresults) {
+      for (auto y : x.second) {
+        std::string f = save_path + "/" + outfile + "_" + std::to_string(i) + ".json";
+        std::ofstream o(f); 
+        i++;
+        json::value g = json::parse(y.second);
+        o << std::setw(4) << g << std::endl; 
+      }
+    }
+  }
+  catch (const Error &e) {
+    std::cout << "Failed : " << e.what() << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
