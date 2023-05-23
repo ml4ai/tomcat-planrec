49a051d20d2d7922020dfc7df911b34322a039cd
Loren Champlin <champlin@email.arizona.edu>
2023-05-23 16:41:46 -0700

    Changed settings

--- test/test_MCTS_planner.cpp
+++ test/test_MCTS_planner.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE TestLoader
+#define BOOST_TEST_MODULE TestMCTSPlanner
 
 #include <boost/test/included/unit_test.hpp>
 #include "../domains/score_functions.h"
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(test_MCTS_planner) {
     auto [domain,problem] = load("../../domains/transport_domain.hddl",
                                  "../../domains/transport_problem.hddl");
 
-    auto results = cppMCTShop(domain,problem,scorers["delivery_one"],30,5,-1,sqrt(2.0),2022);;
+    auto results = cppMCTShop(domain,problem,scorers["delivery_one"],30,1,-1,sqrt(2.0),2022);;
     BOOST_TEST(results.t[results.end].plan.size() == 8);
     BOOST_TEST(results.t[results.end].state.get_facts("at").contains("(at package_0 city_loc_0)"));
     BOOST_TEST(results.t[results.end].state.get_facts("at").contains("(at package_1 city_loc_2)"));
