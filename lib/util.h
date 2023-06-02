889b2549a7f328938aa66e39368f7867498a59a9
Loren Champlin <champlin@email.arizona.edu>
2023-06-02 15:25:26 -0700

    csv writer

--- lib/util.h
+++ lib/util.h
@@ -3,6 +3,7 @@
 #include <boost/json.hpp>
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <iterator>
 #include <random>
 #include <string>
@@ -86,6 +87,38 @@ std::ostream& operator<<(std::ostream& os, const std::variant<T, Ts...>& v) {
     return os;
 }
 
+void write_csv(std::string filename, std::vector<std::pair<std::string, std::vector<double>>> dataset){
+    // Make a CSV file with one or more columns of integer values
+    // Each column of data is represented by the pair <column name, column data>
+    //   as std::pair<std::string, std::vector<int>>
+    // The dataset is represented as a vector of these columns
+    // Note that all columns should be the same size
+
+    // Create an output filestream object
+    std::ofstream myFile(filename);
+
+    // Send column names to the stream
+    for(int j = 0; j < dataset.size(); ++j)
+    {
+        myFile << dataset.at(j).first;
+        if(j != dataset.size() - 1) myFile << ","; // No comma at end of line
+    }
+    myFile << "\n";
+
+    // Send data to the stream
+    for(int i = 0; i < dataset.at(0).second.size(); ++i)
+    {
+        for(int j = 0; j < dataset.size(); ++j)
+        {
+            myFile << dataset.at(j).second.at(i);
+            if(j != dataset.size() - 1) myFile << ","; // No comma at end of line
+        }
+        myFile << "\n";
+    }
+
+    // Close the file
+    myFile.close();
+}
 
 // Define support for printing vectors
 template <typename T>
