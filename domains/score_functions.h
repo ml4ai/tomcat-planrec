38cb6303004cf7d196315cb7c87cf1ed076d919f
Loren Champlin <champlin@email.arizona.edu>
2023-06-15 16:36:48 -0700

    New sar3 score function

--- domains/score_functions.h
+++ domains/score_functions.h
@@ -37,7 +37,44 @@ double travel_one(KnowledgeBase& kb, std::vector<std::string>& plan) {
   return 0.0;
 }
 
+double sar3(KnowledgeBase& kb, std::vector<std::string>& plan) {
+  auto facts = kb.get_facts();
+  double c_vic_count = 0.0;
+  if (facts.find("vic_is_type_C") != facts.end()) {
+    for (auto const& p : facts["vic_is_type_C"]) {
+      c_vic_count += 1.0; 
+    }
+  }
+  double r_vic_count = 0.0;
+  if (facts.find("vic_is_type_A") != facts.end()) {
+    for (auto const& p : facts["vic_is_type_A"]) {
+      r_vic_count += 1.0;
+    }
+  }
+  if (facts.find("vic_is_type_B") != facts.end()) {
+    for (auto const& p : facts["vic_is_type_B"]) {
+      r_vic_count += 1.0;
+    }
+  }
+  double c_res = 0.0;
+  if (facts.find("rescued_c") != facts.end()) {
+    for (auto const& p : facts["rescued_c"]) {
+      c_res += 1.0;
+    }
+  }
+  double r_res = 0.0;
+  if (facts.find("rescued_c") != facts.end()) {
+    for (auto const& p : facts["rescued_r"]) {
+      r_res += 1.0;
+    }
+  }
+  double p_total = c_vic_count*50.0 + r_vic_count*10.0;
+  double points = c_res*50.0 + r_res*10.0;
+  return points/p_total;
+}
+
 Scorers scorers = Scorers({{"delivery_one", delivery_one},
                            {"delivery_one_rec",delivery_one_rec},
                            {"travel_one", travel_one},
+                           {"sar3",sar3},
                            {"simple", simple}});
