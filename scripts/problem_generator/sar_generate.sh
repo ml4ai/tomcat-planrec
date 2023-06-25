e933645be402922b0909bceebe9a7ea5a64548be
Salena Ashton <salena.ashton@gmail.com>
2023-06-24 17:23:47 -0700

    Generates players, victims, marker types, locations, sickbays, victim types and rubble locations. It also initializes these objects with most predicates that are appropriate for the beginning of a mission.

--- scripts/problem_generator/sar_generate.sh
+++ scripts/problem_generator/sar_generate.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 P="3 3 3"
-V="3 3 3"
+V="3 4 5"
 M="1 2 2"
-R="3 3 3"
-X="1 1 3"
+R="3 4 5"
+X="1 1 2"
 
 for i in $(seq 1 3) ; do
-	echo $i
+	echo "problem generated:" $i
 	PP=$(echo $P | cut -d' ' -f $i)
 	VV=$(echo $V | cut -d' ' -f $i)
 	MM=$(echo $M | cut -d' ' -f $i)
 	RR=$(echo $R | cut -d' ' -f $i)
 	XX=$(echo $X | cut -d' ' -f $i)
-    python sar_generator.py -p $PP -v $VV -m $MM -r $RR -x $XX> probs_generated/testProblem$(printf "_%01d-%01d-%01d" $PP $VV $RR).hddl
+    python sar_generator.py -p $PP -v $VV -m $MM -r $RR -x $XX > problem$(printf "_%01d-%01d-%01d" $PP $VV $RR).hddl
 done
--- scripts/problem_generator/sar_generator.py
+++ scripts/problem_generator/sar_generator.py
@@ -23,7 +23,7 @@ Functions needed for this File:
 
 Notes:
     * Right now it initializes with the same number of players, victims and
-    rooms. This is temporary so I can shuffle or randomize the players tending
+    locations. This is temporary so I can shuffle or randomize the players tending
     to different victims.
 
 """
@@ -34,7 +34,7 @@ import sys
 import random
 
 #------------------------------------------------------------------------------
-def get_objects(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
+def get_objects(n_players, n_victims, n_markerTypes, n_locations, n_rubble):
     """
     Purpose:
         Generates objects section of the problem file. The number of each
@@ -44,8 +44,8 @@ def get_objects(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
         n_players: number of players
         n_victims: number of victims
         n_markerTypes: number of markerTyping locations, if they differ from victim
-        n_rooms:  number of rooms that are a final destination
-        n_rubble: Rubble found in rooms. This argument might be temporary.
+        n_locations:  number of locations that are a final destination
+        n_rubble: Rubble found in locations. This argument might be temporary.
 
     Returns:
         A string of objects
@@ -55,7 +55,7 @@ def get_objects(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
             player1 player2 - player
             victim1 victim2 - victim
             markerType1 markerType2 - markerType
-            room1 - location
+            location1 - location
         )
     """
 
@@ -63,7 +63,9 @@ def get_objects(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
     s = "{}\n\t\t{}".format(s, get_obj_line(n_players, "player"))
     s = "{}\n\t\t{}".format(s, get_obj_line(n_victims, "victim"))
     s = "{}\n\t\t{}".format(s, get_obj_line(n_markerTypes, "markerType"))
-    s = "{}\n\t\t{}".format(s, get_obj_line(n_rooms, "room"))
+    s = "{}\n\t\t{}".format(s, "sickbay_A sickbay_B sickbay_C - location")
+    s = "{}\n\t\t{}".format(s, "sos novictim regularvictim criticalvictim threat abrasion boneabrasion rubble_marker - marker_type")
+    s = "{}\n\t\t{}".format(s, get_obj_line(n_locations, "location"))
     s = "{}\n\t\t{}".format(s, get_obj_line(n_rubble, "rubble"))
     return s + "\n\t)"
 
@@ -109,7 +111,7 @@ def get_atom_line(*args):
 
 
 #------------------------------------------------------------------------------
-def get_init(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
+def get_init(n_players, n_victims, n_markerTypes, n_locations, n_rubble):
     """
     Purpose:
         Generate the initial state of the problem file. This initializes
@@ -119,8 +121,8 @@ def get_init(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
         n_players: number of players
         n_victims: number of victims
         n_markerTypes: number of markerTyping locations, if they differ from victim
-        n_rooms:  number of rooms that are a final destination
-        n_rubble: Rubble found in rooms. This argument might be temporary.
+        n_locations:  number of locations that are a final destination
+        n_rubble: Rubble found in locations. This argument might be temporary.
 
     Returns:
         A string of initialized states for the predicates.
@@ -166,30 +168,54 @@ def get_init(n_players, n_victims, n_markerTypes, n_rooms, n_rubble):
                 (diagnosed_B ?arg0 - victim)
                 (diagnosed_C ?arg0 - victim)
                 (marker_at ?arg0 - player ?arg1 - marker_type ?arg2 -   location)
-
     """
+
     s = "\t(:init "
 
-    # For now, assume victim 1 is a critical victim at the beginning.
-    s = "{}\n\t\t{}".format(s, get_atom_line("unconscious", "victim1"))+ "\n"
+    # Assign players to role within the mission
+    s = "{}\n\t\t{}".format(s, get_atom_line("medic", "player1"))
+    s = "{}\n\t\t{}".format(s, get_atom_line("transporter", "player2"))
+    s = "{}\n\t\t{}".format(s, get_atom_line("engineer", "player3"))
 
-    # Randomize players, victims and room locations.
+    # Temporary placeholders for novictim and sos marker types
+    s = "{}\n\t\t{}".format(s, get_atom_line("SOS", "sos"))
+    s = "{}\n\t\t{}".format(s, get_atom_line("novictim", "novictim"))
+
+    # For now, assume victim 1 is a critical victim at the beginning.
+    s = "{}\n\t\t{}".format(s, get_atom_line("unconscious", "victim1")) + "\n"
+
+    # Randomize players, victims and location locations.
     #TODO: Right now it initializes as the same number of players, victims and
         # Rooms. This will need to be changed soon.
     players = list(range(1, n_players + 1))
     victims = list(range(1, n_victims + 1))
-    rooms = list(range(1, n_rooms + 1))
+    rubbles= list(range(1, n_rubble + 1))
+    locations = list(range(1, n_locations + 1))
 
     random.shuffle(players)
     random.shuffle(victims)
-    random.shuffle(rooms)
+    random.shuffle(locations)
 
     for i in range(n_victims):
-        s += "\t\t" + get_atom_line("at", "victim{}".format(victims[i]), "room{}".format(rooms[i])) + "\n"
-        #s = "{}\t\t{}".format(s, get_atom_line("at", "victim{}".format(victims[i]), "room{}".format(rooms[i])))+ "\n"
+        s = "{}\t\t{}".format(s, get_atom_line("at", "victim{}".format(victims[i]), "location{}".format(locations[i]))) + "\n"
 
     for i in range(n_players):
-        s += "\t\t" + get_atom_line("at", "player{}".format(players[i]),  "room{}".format(rooms[i])) + "\n"
+        s = "{}\t\t{}".format(s, get_atom_line("at", "player{}".format(players[i]), "location{}".format(locations[i]))) + "\n"
+
+    for i in range(n_rubble):
+        s = "{}\t\t{}".format(s, get_atom_line("present",
+                                               "rubble{}".format(rubbles[i]), "location{}".format(locations[i]))) + "\n"
+
+    # Randomize the type of victim we have.
+    types = ["A", "B", "C"]   # Three types of victims and sickbays possible.
+    for i in range(n_victims):
+        t = random.choice(types)
+        s = "{}\t\t{}".format(s, get_atom_line("vic_is_type_" + t, "victim{}".format(victims[i]), )) + "\n"
+
+    # Assign victim types to sickbay types.
+    s = "{}\t\t{}".format(s, get_atom_line("sb_is_type_A", "sickbay_A"))
+    s = "{}\n\t\t{}".format(s, get_atom_line("sb_is_type_B", "sickbay_B"))
+    s = "{}\n\t\t{}".format(s, get_atom_line("sb_is_type_C", "sickbay_C"))
 
     return s + "\n\t)"
 
@@ -199,7 +225,7 @@ def main():
     """
     Purpose:
         Problem generation begins here, using command-line arguments and naming
-        the file in a player-victim-room numbering format. This naming
+        the file in a player-victim-location numbering format. This naming
         format should be changed during development.
 
     Arguments:
@@ -215,26 +241,26 @@ def main():
     parser.add_argument("-p", help="players", required=True, type=int)
     parser.add_argument("-v", help="victims", required=True, type=int)
     parser.add_argument("-m", help="markerTypes", required=False, type=int)
-    parser.add_argument("-r", help="rooms", required=True, type=int)
+    parser.add_argument("-r", help="locations", required=True, type=int)
     parser.add_argument("-x", help="rubble", required=False, type=int)
     args = parser.parse_args()
 
     n_players = args.p
     n_victims = args.v
     n_markerTypes = args.m
-    n_rooms = args.r
+    n_locations = args.r
     n_rubble = args.x
 
-    if n_rooms < 1 or n_victims < 1 or n_players < 1:
-        print("You need at least one room, one player and one victim.")
+    if n_locations < 1 or n_victims < 1 or n_players < 1:
+        print("You need at least one location, one player and one victim.")
         exit(1)
 
-    problem_name = "p-{}-{}-{}".format(n_players, n_victims, n_rooms)
+    problem_name = "p-{}-{}-{}".format(n_players, n_victims, n_locations)
 
     print("(define (problem " + problem_name + ")")
     print("\t(:domain sar3)")
-    print(get_objects(n_players, n_victims, n_markerTypes, n_rooms, n_rubble))
-    print(get_init(n_players, n_victims, n_markerTypes, n_rooms, n_rubble))
+    print(get_objects(n_players, n_victims, n_markerTypes, n_locations, n_rubble))
+    print(get_init(n_players, n_victims, n_markerTypes, n_locations, n_rubble))
     print(")")
 
 
